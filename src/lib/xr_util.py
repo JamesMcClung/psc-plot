faaf4dfe0ed654795a2312c57fef027c6bbdce2c
James McClung <james@themcclungs.net>
2025-02-18 12:31:59 -0500

    xr_util: +get_available_steps

--- src/lib/xr_util.py
+++ src/lib/xr_util.py
@@ -6,6 +6,13 @@ import xarray
 ROOT_DIR = pathlib.Path("/Users/james/Code/cc/PSC/psc-runs/psc_shock")
 
 
+def get_available_steps(bp_name: str) -> list[int]:
+    files = ROOT_DIR.glob(f"{bp_name}.*.bp")
+    steps = [int(file.name.split(".")[1]) for file in files]
+    steps.sort()
+    return steps
+
+
 def load_ds(bp_name: str, step: int) -> xarray.Dataset:
     ds = xarray.load_dataset(ROOT_DIR / f"{bp_name}.{step:09}.bp")
     ds = pscpy.decode_psc(ds, ["e", "i"])
