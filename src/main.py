96eb1e429d8f3203c3c93042cfa9891bde8bb20b
James McClung <james@themcclungs.net>
2025-02-18 12:52:01 -0500

    main: make animation

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 from lib import plt_util, xr_util
 
@@ -21,5 +22,18 @@ plt_util.update_cbar(im)
 plt_util.update_title(ax, var, ds.time)
 ax.set_xlabel("y index")
 ax.set_ylabel("z index")
+
+
+def update_anim(step: int):
+    ds = xr_util.load_ds(bp_name, step)
+    im_data = xr_util.get_im_data(ds, var)
+
+    im.set_array(im_data)
+    plt_util.update_title(ax, var, ds.time)
+    plt_util.update_cbar(im)
+    return [im, ax.title]
+
+
+anim = FuncAnimation(fig, update_anim, frames=steps, blit=False)  # FIXME get blitting to work with the title
 
 plt.show()
