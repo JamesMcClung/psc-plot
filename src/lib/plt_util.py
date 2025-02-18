61f75ceef2f36b7f355f85a1c1ba87a7c71c7644
James McClung <james@themcclungs.net>
2025-02-18 12:11:53 -0500

    main, +plt_util: extract update_cbar

---
+++ src/lib/plt_util.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from matplotlib.image import AxesImage
+
+
+def update_cbar(im: AxesImage):
+    im_data = im.get_array()
+    data_min = im_data.min()
+    data_max = im_data.max()
+
+    if data_min >= 0:
+        cmin = 0
+        cmax = data_max
+        cmap = "inferno"
+    elif data_max <= 0:
+        cmin = data_min
+        cmax = 0
+        cmap = "inferno_r"
+    else:
+        cmax = max(abs(im_data.max()), abs(im_data.min()))
+        cmin = -cmax
+        cmap = "RdBu_r"
+
+    im.set_clim(cmin, cmax)
+    im.set_cmap(plt.get_cmap(cmap))
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import pscpy
 import xarray
 import matplotlib.pyplot as plt
-from matplotlib.image import AxesImage
+from lib import plt_util
 
 
 def load_ds(bp_name: str, step: int) -> xarray.Dataset:
@@ -22,30 +22,7 @@ fig, ax = plt.subplots()
 im = ax.imshow(im_data)
 cbar = fig.colorbar(im)
 
-
-def update_cbar(im: AxesImage):
-    im_data = im.get_array()
-    data_min = im_data.min()
-    data_max = im_data.max()
-
-    if data_min >= 0:
-        cmin = 0
-        cmax = data_max
-        cmap = "inferno"
-    elif data_max <= 0:
-        cmin = data_min
-        cmax = 0
-        cmap = "inferno_r"
-    else:
-        cmax = max(abs(im_data.max()), abs(im_data.min()))
-        cmin = -cmax
-        cmap = "RdBu_r"
-
-    im.set_clim(cmin, cmax)
-    im.set_cmap(plt.get_cmap(cmap))
-
-
-update_cbar(im)
+plt_util.update_cbar(im)
 
 ax.set_title(f"{var} (t={ds.time:.2f})")
 ax.set_xlabel("y index")
