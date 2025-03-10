b14ebd322ce22c1e24f177261633bb59807e45d7
James McClung <james@themcclungs.net>
2025-03-10 14:17:09 -0400

    file_util: +PREFIX_TO_SUFFIX map

--- src/lib/file_util.py
+++ src/lib/file_util.py
@@ -1,7 +1,15 @@
 import pathlib
+import typing
 
+type Prefix = typing.Literal["pfd", "pfd_moments", "prt"]
+type Suffix = typing.Literal["bp", "h5"]
 
 ROOT_DIR = pathlib.Path("/Users/james/Code/cc/PSC/psc-runs/psc_shock")
+PREFIX_TO_SUFFIX: dict[Prefix, Suffix] = {
+    "pfd": "bp",
+    "pfd_moments": "bp",
+    "prt": "h5",
+}
 
 
 def get_available_steps(before_step: str, after_step: str) -> list[int]:
