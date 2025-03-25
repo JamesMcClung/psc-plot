cecf5d2e1870091d734aaf783d6c0c0d3cb8c041
James McClung <james@themcclungs.net>
2025-03-25 14:12:36 -0400

    bp_util: +default latex units

--- src/lib/bp_util.py
+++ src/lib/bp_util.py
@@ -8,6 +8,9 @@ from . import file_util
 type BpDim = typing.Literal["x", "y", "z"]
 BP_DIMS: list[BpDim] = list(BpDim.__value__.__args__)
 
+DEFAULT_TIME_UNIT_LATEX = "$\\omega_\\text{pe}^{-1}$"
+DEFAULT_SPACE_UNIT_LATEX = "$d_\\text{e}$"
+
 
 def get_available_steps_bp(prefix: file_util.BpPrefix) -> list[int]:
     return file_util.get_available_steps(f"{prefix}.", ".bp")
