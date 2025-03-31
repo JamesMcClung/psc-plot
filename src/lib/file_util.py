36d565f606dd4387ab0ada413b0e77f5a71ddbe3
James McClung <james@themcclungs.net>
2025-03-31 15:06:49 -0400

    file_util: +lists of valid prefixes

--- src/lib/file_util.py
+++ src/lib/file_util.py
@@ -1,17 +1,17 @@
 import pathlib
 import typing
 
-type BpPrefix = typing.Literal["pfd", "pfd_moments"]
+type BpPrefix = typing.Literal["pfd", "pfd_moments", "gauss", "continuity"]
+BP_PREFIXES: list[BpPrefix] = list(BpPrefix.__value__.__args__)
+
 type H5Prefix = typing.Literal["prt"]
+H5_PREFIXES: list[H5Prefix] = list(H5Prefix.__value__.__args__)
+
 type Prefix = BpPrefix | H5Prefix
 type Suffix = typing.Literal["bp", "h5"]
 
 ROOT_DIR = pathlib.Path("/Users/james/Code/cc/PSC/psc-runs/psc_shock")
-PREFIX_TO_SUFFIX: dict[Prefix, Suffix] = {
-    "pfd": "bp",
-    "pfd_moments": "bp",
-    "prt": "h5",
-}
+PREFIX_TO_SUFFIX: dict[Prefix, Suffix] = {bp_prefix: "bp" for bp_prefix in BP_PREFIXES} | {h5_prefix: "h5" for h5_prefix in H5_PREFIXES}
 
 
 def get_available_steps(before_step: str, after_step: str) -> list[int]:
