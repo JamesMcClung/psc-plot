fb36a1a608d7b5020fa70cebb45934fe6bf60194
James McClung <james@themcclungs.net>
2025-04-01 14:22:56 -0400

    +derived_variable_bp: +DerivedVariableBP

---
+++ src/lib/derived_variables_bp/derived_variable_bp.py
@@ -0,0 +1,22 @@
+import typing
+
+import xarray as xr
+
+
+class DeriveBp(typing.Protocol):
+    def __call__(self, *variables: xr.DataArray) -> xr.DataArray: ...
+
+
+class DerivedVariableBp:
+    def __init__(
+        self,
+        name: str,
+        base_var_names: list[str],
+        derive: DeriveBp,
+    ):
+        self.name = name
+        self.base_var_names = base_var_names
+        self.derive = derive
+
+    def assign_to(self, ds: xr.Dataset):
+        ds[self.name] = self.derive(*(ds[base_var_name] for base_var_name in self.base_var_names))
