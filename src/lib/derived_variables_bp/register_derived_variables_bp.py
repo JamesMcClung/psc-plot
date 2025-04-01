6a60f52d09bee90a9b0606853be749d65ecbb930
James McClung <james@themcclungs.net>
2025-04-01 15:04:55 -0400

    registry: register by prefix

--- src/lib/derived_variables_bp/register_derived_variables_bp.py
+++ src/lib/derived_variables_bp/register_derived_variables_bp.py
@@ -5,6 +5,6 @@ from .registry import derived_variable_bp
 __all__ = []
 
 
-@derived_variable_bp
+@derived_variable_bp("pfd_moments")
 def rho(rho_i: DataArray, rho_e: DataArray) -> DataArray:
     return rho_i + rho_e
--- src/lib/derived_variables_bp/registry.py
+++ src/lib/derived_variables_bp/registry.py
@@ -1,14 +1,18 @@
 import inspect
 
+from ..file_util import BpPrefix
 from .derived_variable_bp import DeriveBp, DerivedVariableBp
 
 __all__ = ["derived_variable_bp", "DERIVED_VARIABLE_BP_REGISTRY"]
 
-DERIVED_VARIABLE_BP_REGISTRY: dict[str, DerivedVariableBp] = {}
+DERIVED_VARIABLE_BP_REGISTRY: dict[BpPrefix, dict[str, DerivedVariableBp]] = {}
 
 
-def derived_variable_bp[F: (function, DeriveBp)](derive_func: F) -> F:
-    name = derive_func.__name__
-    base_var_names = list(inspect.signature(derive_func).parameters)
-    DERIVED_VARIABLE_BP_REGISTRY[name] = DerivedVariableBp(name, base_var_names, derive_func)
-    return derive_func
+def derived_variable_bp(prefix: BpPrefix):
+    def derived_variable_bp_inner[F: (function, DeriveBp)](derive_func: F) -> F:
+        name = derive_func.__name__
+        base_var_names = list(inspect.signature(derive_func).parameters)
+        DERIVED_VARIABLE_BP_REGISTRY.setdefault(prefix, {})[name] = DerivedVariableBp(name, base_var_names, derive_func)
+        return derive_func
+
+    return derived_variable_bp_inner
