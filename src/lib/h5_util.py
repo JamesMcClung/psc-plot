ed59b71e963ae53c4cd7032ec395642a51ab49ca
James McClung <james@themcclungs.net>
2025-04-10 12:38:45 -0400

    h5_util: update to new prt file format

--- src/lib/h5_util.py
+++ src/lib/h5_util.py
@@ -12,10 +12,10 @@ SPECIES: list[Species] = list(Species.__value__.__args__)
 
 
 def get_available_steps_h5(prefix: file_util.H5Prefix) -> list[int]:
-    return file_util.get_available_steps(f"{prefix}.", "_p000000.h5")
+    return file_util.get_available_steps(f"{prefix}.", ".h5")
 
 
 def load_df(prefix: file_util.H5Prefix, step: int) -> pandas.DataFrame:
-    data_path = file_util.ROOT_DIR / f"{prefix}.{step:06}_p000000.h5"
+    data_path = file_util.ROOT_DIR / f"{prefix}.{step:09}.h5"
     df = pandas.read_hdf(data_path, key="particles/p0/1d")
     return df
