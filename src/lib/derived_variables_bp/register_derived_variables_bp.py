58fe9f642ed9b7b4c9950f19d596df40842c5039
James McClung <james@themcclungs.net>
2025-10-21 16:04:39 -0400

    register_derived_variables_bp: +h_cc

--- src/lib/derived_variables_bp/register_derived_variables_bp.py
+++ src/lib/derived_variables_bp/register_derived_variables_bp.py
@@ -1,4 +1,6 @@
-from xarray import DataArray
+import numpy as np
+import pscpy
+from xarray import DataArray, Dataset
 
 from .registry import derived_variable_bp
 
@@ -18,3 +20,15 @@ def error(rho: DataArray, dive: DataArray) -> DataArray:
 @derived_variable_bp("continuity")
 def error(d_rho: DataArray, dt_divj: DataArray) -> DataArray:
     return d_rho + dt_divj
+
+
+@derived_variable_bp("pfd")
+def h2_cc(hx_fc: DataArray, hy_fc: DataArray, hz_fc: DataArray) -> DataArray:
+    h = Dataset({"hx_fc": hx_fc, "hy_fc": hy_fc, "hz_fc": hz_fc})
+    pscpy.auto_recenter(h, "cc", x="periodic", y="pad", z="pad")
+    return h["hx_cc"] ** 2 + h["hy_cc"] ** 2 + h["hz_cc"] ** 2
+
+
+@derived_variable_bp("pfd")
+def h_cc(h2_cc: DataArray) -> DataArray:
+    return np.sqrt(h2_cc)
