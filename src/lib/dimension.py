deb193d4ca295d4bc8428753f61bbaffe3ea95f1
James McClung <james@themcclungs.net>
2025-10-21 16:19:16 -0400

    dimension: +DIMENSIONS

--- src/lib/dimension.py
+++ src/lib/dimension.py
@@ -8,3 +8,11 @@ ELECTRON_SKIN_DEPTH = "d_\\text{e}"
 class Dimension:
     name: str  # latex-formated, sans '$'
     unit: str  # latex-formated, sans '$'
+
+
+DIMENSIONS = {
+    "x": Dimension("x", ELECTRON_SKIN_DEPTH),
+    "y": Dimension("y", ELECTRON_SKIN_DEPTH),
+    "z": Dimension("z", ELECTRON_SKIN_DEPTH),
+    "t": Dimension("t", INVERSE_ELECTRON_PLASMA_FREQUENCY),
+}
