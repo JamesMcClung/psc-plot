9326dd52116f1e4b1c6f858f6d2c922ebb43370f
James McClung <james@themcclungs.net>
2025-10-21 16:23:37 -0400

    dimension: frozen, thus hashable

--- src/lib/dimension.py
+++ src/lib/dimension.py
@@ -4,7 +4,7 @@ INVERSE_ELECTRON_PLASMA_FREQUENCY = "\\omega_\\text{pe}^{-1}"
 ELECTRON_SKIN_DEPTH = "d_\\text{e}"
 
 
-@dataclass
+@dataclass(frozen=True)
 class Dimension:
     name: str  # latex-formated, sans '$'
     unit: str  # latex-formated, sans '$'
