042acaf47b1c7d1cbae86b31bc2be7d98237c888
James McClung <james@themcclungs.net>
2025-10-22 15:29:03 -0400

    plugins_*: auto import

--- src/lib/plugins/plugins_h5/__init__.py
+++ src/lib/plugins/plugins_h5/__init__.py
@@ -1,1 +1,9 @@
-from . import slice, species_filter
+import importlib
+from pathlib import Path
+
+this_file = Path(__file__)
+module_paths = [path for path in this_file.parent.glob("*.py") if path != this_file]
+modules = [path.stem for path in module_paths]
+
+for module in modules:
+    importlib.import_module(f"lib.plugins.plugins_h5.{module}")
