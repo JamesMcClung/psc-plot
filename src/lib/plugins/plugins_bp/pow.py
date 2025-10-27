b50c8e618579bb22f82737c20606a3bcaa4323df
James McClung <james@themcclungs.net>
2025-10-27 10:21:23 -0400

    +pow

---
+++ src/lib/plugins/plugins_bp/pow.py
@@ -0,0 +1,29 @@
+import xarray as xr
+
+from .. import parse_util
+from ..plugin_base import PluginBp
+from ..registry import plugin_parser
+
+
+class Pow(PluginBp):
+    def __init__(self, exponent: float):
+        self.exponent = exponent
+
+    def apply(self, da: xr.DataArray) -> xr.DataArray:
+        return da**self.exponent
+
+    def get_name_fragment(self) -> str:
+        return f"pow_{self.exponent}"
+
+
+POW_FORMAT = "exponent"
+
+
+@plugin_parser(
+    "--pow",
+    metavar=POW_FORMAT,
+    help="raise to the given power",
+)
+def parse_pow(arg: str) -> Pow:
+    exponent = parse_util.parse_number(arg, "exponent", float)
+    return Pow(exponent)
