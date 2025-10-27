e8260b3e34c3518f0a7d28a042e85346136ef8d9
James McClung <james@themcclungs.net>
2025-10-27 11:49:33 -0400

    plugin_base: -get_modified_dep_var_label

--- src/lib/plugins/plugin_base.py
+++ src/lib/plugins/plugin_base.py
@@ -18,8 +18,5 @@ class PluginBp(Plugin[xr.DataArray]):
     def get_modified_title_stem(self, title_stem: str) -> str:
         return title_stem
 
-    def get_modified_dep_var_label(self, label: str) -> str:
-        return label
-
 
 class PluginH5(Plugin[pd.DataFrame]): ...
