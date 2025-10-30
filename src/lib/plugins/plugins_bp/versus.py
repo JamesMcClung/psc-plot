ddc101d0e4641d5e017922b9a8e7b7f524c833a3
James McClung <james@themcclungs.net>
2025-10-30 16:03:48 -0400

    versus: reflect inner plugins in names

--- src/lib/plugins/plugins_bp/versus.py
+++ src/lib/plugins/plugins_bp/versus.py
@@ -11,39 +11,59 @@ from .reduce import Reduce
 class Versus(PluginBp):
     def __init__(self, dim_names: list[str]):
         self.dim_names = dim_names
+        self.cached_inner_plugins: list[PluginBp] | None = None
 
     def apply(self, da: xr.DataArray) -> xr.DataArray:
-        # 1. apply implicit coordinate transforms, as necessary
-        for dim_name in self.dim_names:
-            # 1a. already have the coordinate; do nothing
-            if dim_name in da.dims:
-                continue
+        if self.cached_inner_plugins is None:
+            self.cached_inner_plugins = []
+            # 1. apply implicit coordinate transforms, as necessary
+            for dim_name in self.dim_names:
+                # 1a. already have the coordinate; do nothing
+                if dim_name in da.dims:
+                    continue
 
-            # 1b. need to do a Fourier transform
-            dim = DIMENSIONS[dim_name]
-            f_dim = dim.toggle_fourier()
-            if f_dim.name in da.dims:
-                fourier = Fourier(f_dim.name)
-                da = fourier.apply(da)
-                continue
+                # 1b. need to do a Fourier transform
+                dim = DIMENSIONS[dim_name]
+                f_dim = dim.toggle_fourier()
+                if f_dim.name in da.dims:
+                    fourier = Fourier(f_dim.name)
+                    self.cached_inner_plugins.append(fourier)
+                    da = fourier.apply(da)
+                    continue
 
-            # 1c. need to do a coordinate transform
-            # TODO
+                # 1c. need to do a coordinate transform
+                # TODO
 
-        # 2. reduce remaining dimensions via arithmetic mean
-        for dim_name in da.dims:
-            if dim_name not in self.dim_names:
-                reduce = Reduce(dim_name, "mean")
-                da = reduce.apply(da)
+            # 2. reduce remaining dimensions via arithmetic mean
+            for dim_name in da.dims:
+                if dim_name not in self.dim_names:
+                    reduce = Reduce(dim_name, "mean")
+                    self.cached_inner_plugins.append(reduce)
+                    da = reduce.apply(da)
+        else:
+            for plugin in self.cached_inner_plugins:
+                da = plugin.apply(da)
 
         # 3. transpose to correct dimension order
         da = da.transpose(*self.dim_names)
 
         return da
 
+    def get_modified_dep_var_name(self, dep_var_name):
+        assert self.cached_inner_plugins is not None, "can't modify dep var name—don't know what inner plugins are required yet"
+
+        for p in self.cached_inner_plugins:
+            dep_var_name = p.get_modified_dep_var_name(dep_var_name)
+
+        return dep_var_name
+
     def get_name_fragment(self) -> str:
+        assert self.cached_inner_plugins is not None, "can't generate name fragment—don't know what inner plugins are required yet"
+
+        inner_plugin_fragments = [p.get_name_fragment() for p in self.cached_inner_plugins]
         dims = ",".join(self.dim_names)
-        return f"vs_{dims}"
+        this_fragment = f"vs_{dims}"
+        return "-".join(inner_plugin_fragments + [this_fragment])
 
 
 _VERSUS_FORMAT = "dim_name"
