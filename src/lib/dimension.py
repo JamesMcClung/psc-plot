5df9a2ccbb5016533a65136a1e900ae2b8a9baf2
James McClung <james@themcclungs.net>
2025-11-03 15:39:50 -0500

    dimension: +CartesianToPolar

--- src/lib/dimension.py
+++ src/lib/dimension.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
+
+import numpy as np
+import numpy.typing as npt
 
 INVERSE_ELECTRON_PLASMA_FREQUENCY = "\\omega_\\text{pe}^{-1}"
 ELECTRON_SKIN_DEPTH = "d_\\text{e}"
@@ -38,6 +42,32 @@ class Dimension:
 
     def is_fourier(self) -> bool:
         return self.name.startswith(FOURIER_NAME_PREFIX)
+
+
+class Transform2D(ABC):
+    @abstractmethod
+    def apply[T: float | npt.NDArray[np.float64]](self, c1: T, c2: T) -> tuple[T, T]: ...
+
+
+class CartesianToPolar(Transform2D):
+    def __init__(self, dim_x: Dimension, dim_y: Dimension):
+        if dim_x.unit != dim_y.unit:
+            raise ValueError("Incompatible units for coordinate transform")
+
+        self.dim_x = dim_x
+        self.dim_y = dim_y
+        self.dim_r = Dimension("r", dim_x.unit)
+        self.dim_theta = Dimension("\\theta", RADIAN)
+
+    def apply[T: float | npt.NDArray[np.float64]](self, x: T, y: T) -> tuple[T, T]:
+        r = (x**2 + y**2) ** 0.5
+        theta = np.arctan2(y, x)
+        return (r, theta)
+
+    def inverse[T: float | npt.NDArray[np.float64]](self, r: T, theta: T) -> tuple[T, T]:
+        x = r * np.cos(theta)
+        y = r * np.sin(theta)
+        return (x, y)
 
 
 DIMENSIONS: dict[str, Dimension] = {}
