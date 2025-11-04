4a214cdb8e9b89d837e3218e6a274628b95c46ff
James McClung <james@themcclungs.net>
2025-11-04 11:16:29 -0500

    fourier; *: take Dimension

--- src/lib/plugins/plugins_bp/fourier.py
+++ src/lib/plugins/plugins_bp/fourier.py
@@ -2,40 +2,39 @@ import numpy as np
 import xarray as xr
 import xrft
 
-from ...dimension import DIMENSIONS
+from ...dimension import DIMENSIONS, Dimension
 from .. import parse_util
 from ..plugin_base import PluginBp
 from ..registry import plugin_parser
 
 
 class Fourier(PluginBp):
-    def __init__(self, dim_name: str):
-        self.dim_name = dim_name
+    def __init__(self, dim: Dimension):
+        self.dim = dim
 
     def apply(self, da: xr.DataArray) -> xr.DataArray:
         temp_prefix = "temp_"
 
-        dim = DIMENSIONS[self.dim_name]
-        f_dim = dim.toggle_fourier()
+        f_dim = self.dim.toggle_fourier()
 
         # multiply/or divide coords by 2pi to go from frequency <-> angular frequency
 
-        if dim.is_fourier():
-            da = da.assign_coords({dim.name.plain: da.coords[dim.name.plain] / (2 * np.pi)})
-            da = xrft.ifft(da, dim=dim.name.plain, prefix=temp_prefix, lag=0.0)
-            da = da.rename({temp_prefix + dim.name.plain: f_dim.name.plain})
+        if self.dim.is_fourier():
+            da = da.assign_coords({self.dim.name.plain: da.coords[self.dim.name.plain] / (2 * np.pi)})
+            da = xrft.ifft(da, dim=self.dim.name.plain, prefix=temp_prefix, lag=0.0)
+            da = da.rename({temp_prefix + self.dim.name.plain: f_dim.name.plain})
         else:
-            da = xrft.fft(da, dim=dim.name.plain, prefix=temp_prefix)
-            da = da.rename({temp_prefix + dim.name.plain: f_dim.name.plain})
+            da = xrft.fft(da, dim=self.dim.name.plain, prefix=temp_prefix)
+            da = da.rename({temp_prefix + self.dim.name.plain: f_dim.name.plain})
             da = da.assign_coords({f_dim.name.plain: da.coords[f_dim.name.plain] * (2 * np.pi)})
 
         return da
 
     def get_name_fragment(self) -> str:
-        return f"fourier_{self.dim_name}"
+        return f"fourier_{self.dim.name.plain}"
 
     def get_modified_dep_var_name(self, title_stem: str) -> str:
-        return f"\\mathcal{{F}}_{{{self.dim_name}}}[{title_stem}]"
+        return f"\\mathcal{{F}}_{{{self.dim.name.latex}}}[{title_stem}]"
 
 
 FOURIER_FORMAT = "dim_name"
@@ -51,5 +50,6 @@ def parse_fourier(arg: str) -> Fourier:
     dim_name = arg
 
     parse_util.check_value(dim_name, "dim_name", DIMENSIONS)
+    dim = DIMENSIONS[dim_name]
 
-    return Fourier(dim_name)
+    return Fourier(dim)
