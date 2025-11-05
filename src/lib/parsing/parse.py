2fa4547163c66eccf4bbffc13079d0b75b38012e
James McClung <james@themcclungs.net>
2025-11-05 15:49:23 -0500

    field_args; *: rename from args_bp

--- src/lib/parsing/parse.py
+++ src/lib/parsing/parse.py
@@ -1,6 +1,6 @@
 import argparse
 
-from . import args_base, args_bp, args_h5
+from . import args_base, args_h5, field_args
 
 __all__ = ["get_parsed_args"]
 
@@ -9,7 +9,7 @@ def _get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(prog="psc-plot")
     subparsers = args_base.add_subparsers(parser)
 
-    args_bp.add_subparsers_bp(subparsers)
+    field_args.add_subparsers_bp(subparsers)
     args_h5.add_subparsers_h5(subparsers)
 
     return parser
