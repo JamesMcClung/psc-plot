07f61fefafa42531dbf2275c7bc3fd827768343f
James McClung <james@themcclungs.net>
2025-11-05 16:42:27 -0500

    init: fix all

--- src/lib/adaptors/__init__.py
+++ src/lib/adaptors/__init__.py
@@ -4,4 +4,4 @@ from .adaptor_base import FieldAdaptor, ParticleAdaptor
 from .pipeline import FieldPipeline, ParticlePipeline
 from .registry import FIELD_ADAPTORS, PARTICLE_ADAPTORS
 
-__all__ = ["FieldAdaptor", "ParticleAdaptor", "FIELD_ADAPTORS", "PARTICLE_ADAPTORS", "FieldPipeline", ParticlePipeline]
+__all__ = ["FieldAdaptor", "ParticleAdaptor", "FIELD_ADAPTORS", "PARTICLE_ADAPTORS", "FieldPipeline", "ParticlePipeline"]
