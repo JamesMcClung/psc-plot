b64f1dfdc1cd3c991365c01e4f6c00edc603896e
James McClung <james@themcclungs.net>
2025-11-06 15:17:53 -0500

    +fit

---
+++ src/lib/parsing/fit.py
@@ -0,0 +1,8 @@
+# TODO make this a plot plugin (and make plot plugins a thing)
+
+
+class Fit:
+    def __init__(self, arg: str):
+        # TODO proper error handling
+        # TODO actually parse different options for fits
+        pass
