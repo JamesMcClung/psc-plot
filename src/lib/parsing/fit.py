250b0a0554acbdc6240a341e71c17dd99c5b8b16
James McClung <james@themcclungs.net>
2025-11-06 15:32:11 -0500

    fit: return line

--- src/lib/parsing/fit.py
+++ src/lib/parsing/fit.py
@@ -1,5 +1,6 @@
 import xarray as xr
 from matplotlib.axes import Axes
+from matplotlib.lines import Line2D
 
 # TODO make this a plot plugin (and make plot plugins a thing)
 
@@ -10,6 +11,7 @@ class Fit:
         # TODO actually parse different options for fits
         pass
 
-    def plot_fit(self, ax: Axes, da: xr.DataArray):
+    def plot_fit(self, ax: Axes, da: xr.DataArray) -> Line2D:
         # TODO actually do a fit
-        ax.plot(da.coords[da.dims[0]], da, "o")
+        [fit_line] = ax.plot(da.coords[da.dims[0]], da, "o", label="hi")
+        return fit_line
