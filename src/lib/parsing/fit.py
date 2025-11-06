fc07d38901e56248bb6a462d1677f60b13f98e34
James McClung <james@themcclungs.net>
2025-11-06 15:21:30 -0500

    fit: +plot_fit

--- src/lib/parsing/fit.py
+++ src/lib/parsing/fit.py
@@ -1,3 +1,6 @@
+import xarray as xr
+from matplotlib.axes import Axes
+
 # TODO make this a plot plugin (and make plot plugins a thing)
 
 
@@ -6,3 +9,7 @@ class Fit:
         # TODO proper error handling
         # TODO actually parse different options for fits
         pass
+
+    def plot_fit(self, ax: Axes, da: xr.DataArray):
+        # TODO actually do a fit
+        ax.plot(da.coords[da.dims[0]], da, "o")
