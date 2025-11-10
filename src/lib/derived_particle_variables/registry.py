bbcd54e0e697c60cf82450a0705f04d8f2a04e03
James McClung <james@themcclungs.net>
2025-11-10 13:19:17 -0500

    registry: +energy vars

--- src/lib/derived_particle_variables/registry.py
+++ src/lib/derived_particle_variables/registry.py
@@ -23,3 +23,38 @@ def pzx(pz: Series, px: Series) -> Series:
 @derived_particle_variable("prt")
 def anisotropy_y_zx(py: Series, pzx: Series) -> Series:
     return py / pzx
+
+
+@derived_particle_variable("prt")
+def w_x(m: Series, px: Series) -> Series:
+    return 0.5 * m * px**2
+
+
+@derived_particle_variable("prt")
+def w_y(m: Series, py: Series) -> Series:
+    return 0.5 * m * py**2
+
+
+@derived_particle_variable("prt")
+def w_z(m: Series, pz: Series) -> Series:
+    return 0.5 * m * pz**2
+
+
+@derived_particle_variable("prt")
+def w_xy(m: Series, px: Series, py: Series) -> Series:
+    return 0.5 * m * (px**2 + py**2)
+
+
+@derived_particle_variable("prt")
+def w_yz(m: Series, py: Series, pz: Series) -> Series:
+    return 0.5 * m * (py**2 + pz**2)
+
+
+@derived_particle_variable("prt")
+def w_zx(m: Series, pz: Series, px: Series) -> Series:
+    return 0.5 * m * (pz**2 + px**2)
+
+
+@derived_particle_variable("prt")
+def w_xyz(m: Series, px: Series, py: Series, pz: Series) -> Series:
+    return 0.5 * m * (px**2 + py**2 + pz**2)
