3660e33faa09d1b8a0e5694a3a24155473a79abb
James McClung <james@themcclungs.net>
2025-11-17 11:27:02 -0500

    field_source: +FieldSourceWithPipeline

--- src/lib/field_source.py
+++ src/lib/field_source.py
@@ -2,7 +2,20 @@ from abc import ABC, abstractmethod
 
 import xarray as xr
 
+from .adaptors.pipeline import FieldPipeline
+
 
 class FieldSource(ABC):
     @abstractmethod
     def get(self, steps: list[int]) -> xr.DataArray: ...
+
+
+class FieldSourceWithPipeline(FieldSource):
+    def __init__(self, source: FieldSource, pipeline: FieldPipeline):
+        self.source = source
+        self.pipeline = pipeline
+
+    def get(self, steps: list[int]) -> xr.DataArray:
+        da = self.source.get(steps)
+        da = self.pipeline.apply(da)
+        return da
