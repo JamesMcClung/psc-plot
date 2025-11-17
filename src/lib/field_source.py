f6504dcf5eeb78b8d9024b74f6598b25a7b46029
James McClung <james@themcclungs.net>
2025-11-17 10:37:12 -0500

    field_source: +FieldLoader

--- src/lib/field_source.py
+++ src/lib/field_source.py
@@ -12,3 +12,12 @@ def load_field_variable(prefix: file_util.FieldPrefix, step: int, var_name: str)
     derive_field_variable(ds, var_name, prefix)
 
     return ds[var_name]
+
+
+class FieldLoader:
+    def __init__(self, prefix: file_util.FieldPrefix, var_name: str):
+        self.prefix = prefix
+        self.var_name = var_name
+
+    def get_step(self, step: int) -> xr.DataArray:
+        return load_field_variable(self.prefix, step, self.var_name)
