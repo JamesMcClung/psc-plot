b681d110490006e4e9ca85c57fa39d411076f3f3
James McClung <james@themcclungs.net>
2025-11-18 15:57:55 -0500

    particle_util: set attrs

--- src/lib/particle_util.py
+++ src/lib/particle_util.py
@@ -1,8 +1,9 @@
 import typing
 
+import h5py
 import pandas
 
-from . import file_util
+from . import field_util, file_util
 
 type PrtVariable = typing.Literal["x", "y", "z", "px", "py", "pz", "q", "m", "w", "id", "tag"]
 PRT_VARIABLES: list[PrtVariable] = list(PrtVariable.__value__.__args__)
@@ -17,5 +18,20 @@ def get_available_particle_steps(prefix: file_util.ParticlePrefix) -> list[int]:
 
 def load_df(prefix: file_util.ParticlePrefix, step: int) -> pandas.DataFrame:
     data_path = file_util.ROOT_DIR / f"{prefix}.{step:09}.h5"
-    df = pandas.read_hdf(data_path, key="particles/p0/1d")
+    df = pandas.read_hdf(data_path, key="particles/p0/1d")  # using h5py.File not yet supported
+    with h5py.File(data_path) as file:
+        if "time" in file.keys():
+            time = file["time"][()]
+            corner = file["corner"][:]
+            length = file["length"][:]
+            gdims = file["gdims"][:]
+        else:
+            ds = field_util.load_ds("pfd", step)
+            time = ds.time
+            corner = ds.corner
+            length = ds.length
+            gdims = [len(ds.x), len(ds.y), len(ds.z)]
+
+        df.attrs = {"time": time, "corner": corner, "length": length, "gdims": gdims}
+
     return df
