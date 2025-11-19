b5ba1f97d72029a213f85e664c0ac49671bb6868
James McClung <james@themcclungs.net>
2025-11-19 14:51:02 -0500

    compatability: include module in err message

--- src/lib/data/compatability.py
+++ src/lib/data/compatability.py
@@ -20,4 +20,6 @@ def are_compatible(producer: ProducesData, consumer: ConsumesData) -> bool:
 
 def require_compatible(producer: ProducesData, consumer: ConsumesData):
     if not are_compatible(producer, consumer):
-        raise DataError(f"{producer} produces type {producer.get_output_data_type().__name__}, but feeds into {consumer} which only accepts type {consumer.get_input_data_type().__name__}")
+        output_type = producer.get_output_data_type()
+        input_type = consumer.get_input_data_type()
+        raise DataError(f"{producer} produces type {output_type.__module__}.{output_type.__name__}, but feeds into {consumer} which only accepts type {input_type.__module__}.{input_type.__name__}")
