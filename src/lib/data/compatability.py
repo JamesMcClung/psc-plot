f60be42ebdc0705758bdb643ec559003e795a318
James McClung <james@themcclungs.net>
2025-11-24 13:57:15 -0500

    compatability: +ensure_type

--- src/lib/data/compatability.py
+++ src/lib/data/compatability.py
@@ -14,6 +14,12 @@ class ProducesData(ABC):
 class DataError(Exception): ...
 
 
+def ensure_type[Data](consumer_name: str, data: Data, *allowed_types: type):
+    if not isinstance(data, allowed_types):
+        names_of_types = ", ".join(f"{t.__module__}.{t.__name__}" for t in allowed_types)
+        raise DataError(f"{consumer_name} accepts only the following types: [{names_of_types}], but received data of type {data.__class__.__module__}.{data.__class__}")
+
+
 def are_compatible(producer: ProducesData, consumer: ConsumesData) -> bool:
     return issubclass(producer.get_output_data_type(), consumer.get_input_data_type())
 
