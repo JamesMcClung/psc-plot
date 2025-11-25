c60184487539dcb2fbe84abd4531e26720d41343
James McClung <james@themcclungs.net>
2025-11-25 09:10:39 -0500

    versus: -cached_inner_adaptors

--- src/lib/data/adaptors/field_adaptors/versus.py
+++ src/lib/data/adaptors/field_adaptors/versus.py
@@ -15,40 +15,32 @@ class Versus(Adaptor):
         self.spatial_dims = spatial_dims
         self.time_dim = time_dim
         self.all_dims = spatial_dims + ([time_dim] if time_dim else [])
-        self.cached_inner_adaptors: list[Adaptor] | None = None
 
     def apply(self, da: xr.DataArray) -> xr.DataArray:
         ensure_type(self.__class__.__name__, da, xr.DataArray)
 
-        if self.cached_inner_adaptors is None:
-            self.cached_inner_adaptors = []
-            # 1. apply implicit coordinate transforms, as necessary
-            for dim_name in self.all_dims:
-                # 1a. already have the coordinate; do nothing
-                if dim_name in da.dims:
-                    continue
+        # 1. apply implicit coordinate transforms, as necessary
+        for dim_name in self.all_dims:
+            # 1a. already have the coordinate; do nothing
+            if dim_name in da.dims:
+                continue
 
-                # 1b. need to do a Fourier transform
-                dim = DIMENSIONS[dim_name]
-                f_dim = dim.toggle_fourier()
-                if f_dim.name.plain in da.dims:
-                    fourier = Fourier(f_dim)
-                    self.cached_inner_adaptors.append(fourier)
-                    da = fourier.apply(da)
-                    continue
+            # 1b. need to do a Fourier transform
+            dim = DIMENSIONS[dim_name]
+            f_dim = dim.toggle_fourier()
+            if f_dim.name.plain in da.dims:
+                fourier = Fourier(f_dim)
+                da = fourier.apply(da)
+                continue
 
-                # 1c. need to do a coordinate transform
-                # TODO
+            # 1c. need to do a coordinate transform
+            # TODO
 
-            # 2. reduce remaining dimensions via arithmetic mean
-            for dim_name in da.dims:
-                if dim_name not in self.all_dims:
-                    reduce = Reduce(dim_name, "mean")
-                    self.cached_inner_adaptors.append(reduce)
-                    da = reduce.apply(da)
-        else:
-            for adaptor in self.cached_inner_adaptors:
-                da = adaptor.apply(da)
+        # 2. reduce remaining dimensions via arithmetic mean
+        for dim_name in da.dims:
+            if dim_name not in self.all_dims:
+                reduce = Reduce(dim_name, "mean")
+                da = reduce.apply(da)
 
         # let the animator take it from here
         da.attrs[SPATIAL_DIMS_KEY] = self.spatial_dims
