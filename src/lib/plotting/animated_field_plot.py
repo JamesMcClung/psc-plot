05a611b077cec4e6cf44df18b8da743a8471bbf5
James McClung <james@themcclungs.net>
2025-11-28 15:56:41 -0500

    +animated_plot: extract AnimatedPlot

--- src/lib/plotting/animated_field_plot.py
+++ src/lib/plotting/animated_field_plot.py
@@ -1,64 +1,20 @@
 from __future__ import annotations
 
-import typing
-from abc import abstractmethod
-from pathlib import Path
-
-import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
-from matplotlib.animation import FuncAnimation
 from matplotlib.projections.polar import PolarAxes
 
-from lib.data.keys import SPATIAL_DIMS_KEY, TIME_DIM_KEY, VAR_LATEX_KEY
+from lib.data.keys import TIME_DIM_KEY, VAR_LATEX_KEY
 from lib.dimension import DIMENSIONS
 from lib.parsing.fit import Fit
 from lib.plotting import plt_util
-from lib.plotting.plot import Plot
+from lib.plotting.animated_plot import AnimatedPlot
 
 
-class AnimatedFieldPlot(Plot[xr.DataArray]):
-    def __init__(
-        self,
-        data: xr.DataArray,
-        *,
-        scales: list[plt_util.Scale] = [],
-        subplot_kw: dict[str, typing.Any] = {},
-    ):
-        super().__init__(data)
-        self.spatial_dims: list[str] = self.data.attrs[SPATIAL_DIMS_KEY]
-        self.time_dim: str = self.data.attrs[TIME_DIM_KEY]
-        self.scales = scales + ["linear"] * (1 + len(self.spatial_dims) - len(scales))
-        nframes = len(self.data.coords[self.time_dim])
-
-        self.fig, self.ax = plt.subplots(subplot_kw=subplot_kw)
-        self._initialized = False
-
-        # FIXME get blitting to work with the title
-        # note: blitting doesn't seem to affect saved animations, only ones displayed with plt.show
-        self.anim = FuncAnimation(self.fig, self._update_fig, frames=nframes, blit=False)
-
-    @abstractmethod
-    def _init_fig(self): ...
-
-    @abstractmethod
-    def _update_fig(self, frame: int): ...
-
-    def _initialize(self):
-        if not self._initialized:
-            self._init_fig()
-            self._initialized = True
-
-    def show(self):
-        self._initialize()
-        plt.show()
-
-    def _get_save_ext(self):
-        return ".mp4"
-
-    def _save_to_path(self, path: Path):
-        self._initialize()
-        self.anim.save(path)
+class AnimatedFieldPlot(AnimatedPlot[xr.DataArray]):
+    @classmethod
+    def _get_nframes(cls, data: xr.DataArray) -> int:
+        return len(data.coords[data.attrs[TIME_DIM_KEY]])
 
     def _get_data_at_frame(self, frame: int) -> xr.DataArray:
         return self.data.isel({self.time_dim: frame})
---
+++ src/lib/plotting/animated_plot.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+import typing
+from abc import abstractmethod
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+from lib.data.keys import SPATIAL_DIMS_KEY, TIME_DIM_KEY
+from lib.plotting import plt_util
+from lib.plotting.plot import DataWithAttrs, Plot
+
+
+class AnimatedPlot[Data: DataWithAttrs](Plot[Data]):
+    def __init__(
+        self,
+        data: Data,
+        *,
+        scales: list[plt_util.Scale] = [],
+        subplot_kw: dict[str, typing.Any] = {},
+    ):
+        super().__init__(data)
+        self.spatial_dims: list[str] = self.data.attrs[SPATIAL_DIMS_KEY]
+        self.time_dim: str = self.data.attrs[TIME_DIM_KEY]
+        self.scales = scales + ["linear"] * (1 + len(self.spatial_dims) - len(scales))
+
+        self.fig, self.ax = plt.subplots(subplot_kw=subplot_kw)
+        self._initialized = False
+
+        # FIXME get blitting to work with the title
+        # note: blitting doesn't seem to affect saved animations, only ones displayed with plt.show
+        self.anim = FuncAnimation(self.fig, self._update_fig, frames=self._get_nframes(self.data), blit=False)
+
+    @classmethod
+    @abstractmethod
+    def _get_nframes(cls, data: Data) -> int:
+        """Calculate the number of frames based on the given data"""
+
+    @abstractmethod
+    def _init_fig(self): ...
+
+    @abstractmethod
+    def _update_fig(self, frame: int): ...
+
+    def _initialize(self):
+        if not self._initialized:
+            self._init_fig()
+            self._initialized = True
+
+    def show(self):
+        self._initialize()
+        plt.show()
+
+    def _get_save_ext(self):
+        return ".mp4"
+
+    def _save_to_path(self, path: Path):
+        self._initialize()
+        self.anim.save(path)
