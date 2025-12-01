bdfc3baf1e8369523c98299b2bc7ea801c49c6e5
James McClung <james@themcclungs.net>
2025-12-01 17:27:16 -0500

    compile: don't track dims

--- src/lib/data/compile.py
+++ src/lib/data/compile.py
@@ -5,16 +5,11 @@ from lib.data.source import DataSource, DataSourceWithPipeline
 
 
 def compile_source(loader: DataSource, adaptors: list[Adaptor]) -> DataSource:
-    spatial_dims = ["y", "z"]
-    time_dim: str = "t"
-
     for adaptor in adaptors:
         if isinstance(adaptor, Versus):
-            spatial_dims = adaptor.spatial_dims
-            time_dim = adaptor.time_dim
             break
     else:
-        adaptors.append(Versus(spatial_dims, time_dim, color_dim=None))
+        adaptors.append(Versus(["y", "z"], "t", color_dim=None))
 
     pipeline = Pipeline(*adaptors)
     source = DataSourceWithPipeline(loader, pipeline)
