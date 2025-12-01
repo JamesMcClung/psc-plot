a547196ea64a31e3c5524d996858feac92a13fff
James McClung <james@themcclungs.net>
2025-12-01 13:46:37 -0500

    keys: +COLOR_DIM_KEY

--- src/lib/data/keys.py
+++ src/lib/data/keys.py
@@ -1,5 +1,6 @@
 SPATIAL_DIMS_KEY = "spatial_dims"
 TIME_DIM_KEY = "time_dim"
+COLOR_DIM_KEY = "color_dim"
 VAR_LATEX_KEY = "var_latex"
 NAME_FRAGMENTS_KEY = "name_fragments"
 DEPENDENT_VAR_KEY = "dep_var"
