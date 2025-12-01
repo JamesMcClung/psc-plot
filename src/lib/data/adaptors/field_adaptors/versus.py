d9b066debe716bdb457efa4819904c6dd6cb36ba
James McClung <james@themcclungs.net>
2025-12-01 13:45:48 -0500

    versus: +color_dim

--- src/lib/data/adaptors/field_adaptors/versus.py
+++ src/lib/data/adaptors/field_adaptors/versus.py
@@ -5,6 +5,7 @@ from ....dimension import DIMENSIONS
 from ...adaptor import Adaptor
 from ...compatability import ensure_type, get_allowed_types
 from ...keys import (
+    COLOR_DIM_KEY,
     DEPENDENT_VAR_KEY,
     NAME_FRAGMENTS_KEY,
     SPATIAL_DIMS_KEY,
@@ -17,10 +18,11 @@ from .reduce import Reduce
 
 
 class Versus(Adaptor):
-    def __init__(self, spatial_dims: list[str], time_dim: str | None):
+    def __init__(self, spatial_dims: list[str], time_dim: str | None, color_dim: str | None):
         self.spatial_dims = spatial_dims
         self.time_dim = time_dim
-        self.all_dims = spatial_dims + ([time_dim] if time_dim else [])
+        self.color_dim = color_dim
+        self.all_dims = spatial_dims + ([time_dim] if time_dim else []) + ([color_dim] if color_dim else [])
 
     def apply[T: xr.DataArray | pd.DataFrame](self, da: T) -> T:
         ensure_type(self.__class__.__name__, da, *get_allowed_types(T))
@@ -64,6 +66,7 @@ class Versus(Adaptor):
         # let the animator take it from here
         da.attrs[SPATIAL_DIMS_KEY] = self.spatial_dims
         da.attrs[TIME_DIM_KEY] = self.time_dim
+        da.attrs[COLOR_DIM_KEY] = self.color_dim
         da.attrs[NAME_FRAGMENTS_KEY] = name_frags_before + self.get_name_fragments()
 
         return da
@@ -73,11 +76,14 @@ class Versus(Adaptor):
         dims = ",".join(self.spatial_dims)
         if self.time_dim:
             dims += f";time={self.time_dim}"
+        if self.color_dim:
+            dims += f";color={self.color_dim}"
         return [f"vs_{dims}"]
 
 
 _TIME_PREFIX = "time="
-_VERSUS_FORMAT = f"dim_name | {_TIME_PREFIX}[dim_name]"
+_COLOR_PREFIX = "color="
+_VERSUS_FORMAT = f"dim_name | {_TIME_PREFIX}[dim_name] | {_COLOR_PREFIX}dim_name"
 
 
 @adaptor_parser(
@@ -90,12 +96,16 @@ _VERSUS_FORMAT = f"dim_name | {_TIME_PREFIX}[dim_name]"
 def parse_versus(args: list[str]) -> Versus:
     spatial_dims = []
     time_dim = "t"
+    color_dim = None
     for arg in args:
         if arg.startswith(_TIME_PREFIX):
             time_dim = arg.removeprefix(_TIME_PREFIX) or None
             parse_util.check_optional_identifier(time_dim, "time dim_name")
+        elif arg.startswith(_COLOR_PREFIX):
+            color_dim = arg.removeprefix(_COLOR_PREFIX) or None
+            parse_util.check_identifier(color_dim, "color dim_name")
         else:
             parse_util.check_identifier(arg, "dim_name")
             spatial_dims.append(arg)
 
-    return Versus(spatial_dims, time_dim)
+    return Versus(spatial_dims, time_dim, color_dim)
