d96680fb3cadacb5403519efd7eb8babc03ff26d
James McClung <james@themcclungs.net>
2025-12-01 11:53:13 -0500

    fit: support DataFrame

--- src/lib/parsing/fit.py
+++ src/lib/parsing/fit.py
@@ -1,8 +1,12 @@
 import numpy as np
+import pandas as pd
 import scipy.stats as stats
 import xarray as xr
 from matplotlib.axes import Axes
 from matplotlib.lines import Line2D
+
+from lib.data.adaptors.particle_adaptors.slice import Slice
+from lib.data.keys import DEPENDENT_VAR_KEY, SPATIAL_DIMS_KEY
 
 from ..data.adaptors.field_adaptors.pos_slice import PosSlice
 
@@ -18,22 +22,20 @@ class Fit:
         self.min_x = float(min_x)
         self.max_x = float(max_x)
 
-    def plot_fit(self, ax: Axes, da: xr.DataArray) -> Line2D:
-        fit_da, label = self._get_fit_data(da)
+    def plot_fit(self, ax: Axes, data: xr.DataArray | pd.DataFrame) -> Line2D:
+        fit_da, label = self._get_fit_data(data)
         [fit_line] = ax.plot(fit_da.coords[fit_da.dims[0]], fit_da, "--", label=label)
         return fit_line
 
-    def update_fit(self, da: xr.DataArray, line: Line2D):
-        fit_da, label = self._get_fit_data(da)
+    def update_fit(self, data: xr.DataArray | pd.DataFrame, line: Line2D):
+        fit_da, label = self._get_fit_data(data)
         line.set_data(fit_da.coords[fit_da.dims[0]], fit_da)
         line.set_label(label)
 
-    def _get_fit_data(self, da: xr.DataArray) -> tuple[xr.DataArray, str]:
-        slicer = PosSlice(da.dims[0], self.min_x, self.max_x)
-        da = slicer.apply(da)
-
-        x_log = np.log(da.coords[da.dims[0]])
-        y_log = np.log(da)
+    def _get_fit_data(self, data: xr.DataArray | pd.DataFrame) -> tuple[xr.DataArray, str]:
+        x_data, y_data = self._get_xy_data(data)
+        x_log = np.log(x_data)
+        y_log = np.log(y_data)
 
         [slope, intercept, rvalue, *_] = stats.linregress(x_log, y_log)
 
@@ -43,3 +45,13 @@ class Fit:
         label = f"$\\gamma={-slope:.3f}$ ($r^2={rvalue**2:.3f}$)"
 
         return y_fit, label
+
+    def _get_xy_data(self, data: xr.DataArray | pd.DataFrame) -> tuple[np.ndarray, np.ndarray]:
+        if isinstance(data, xr.DataArray):
+            slicer = PosSlice(data.dims[0], self.min_x, self.max_x)
+            data = slicer.apply(data)
+            return (data.coords[data.dims[0]], data)
+        elif isinstance(data, pd.DataFrame):
+            slicer = Slice(DEPENDENT_VAR_KEY, self.min_x, self.max_x)
+            data = slicer.apply(data)
+            return (data[DEPENDENT_VAR_KEY], data[data.attrs[SPATIAL_DIMS_KEY][0]])
