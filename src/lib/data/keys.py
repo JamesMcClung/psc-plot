e7128e879991b17fbe5eaf4dbd970573f38b1187
James McClung <james@themcclungs.net>
2025-12-02 10:49:39 -0500

    keys: +COORDS_KEY

--- src/lib/data/keys.py
+++ src/lib/data/keys.py
@@ -3,4 +3,7 @@ TIME_DIM_KEY = "time_dim"
 COLOR_DIM_KEY = "color_dim"
 VAR_LATEX_KEY = "var_latex"
 NAME_FRAGMENTS_KEY = "name_fragments"
+
+# dataframe keys
 DEPENDENT_VAR_KEY = "dep_var"
+COORDS_KEY = "coords"
