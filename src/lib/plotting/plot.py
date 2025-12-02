89356138a74d5be7df32cca173728df00362d861
James McClung <james@themcclungs.net>
2025-12-02 10:45:31 -0500

    plot: print on save

--- src/lib/plotting/plot.py
+++ src/lib/plotting/plot.py
@@ -25,3 +25,4 @@ class Plot[Data: DataWithAttrs](ABC):
     def save(self, path_override: Path | None = None):
         path = path_override or "-".join(self.data.attrs[NAME_FRAGMENTS_KEY]) + self._get_save_ext()
         self._save_to_path(path)
+        print(f"wrote to {path}")
