89d5a4daccbaabddfc8e3ff93a657d2aa0b2aa20
James McClung <james@themcclungs.net>
2025-12-04 14:54:05 -0500

    registry: fix div_h

--- src/lib/derived_field_variables/registry.py
+++ src/lib/derived_field_variables/registry.py
@@ -68,4 +68,25 @@ def h_cc(h2_cc: DataArray) -> DataArray:
 
 @derived_field_variable("pfd")
 def div_h_cc(hx_fc: DataArray, hy_fc: DataArray, hz_fc: DataArray) -> DataArray:
-    return hx_fc.differentiate("x") + hy_fc.differentiate("y") + hz_fc.differentiate("z")
+    coords = hx_fc.coords  # they should all have the same coords
+    dims = {"x", "y", "z"}
+    noninvariant_dims = {dim for dim in dims if len(coords[dim]) > 1}
+    h_comps = {"x": hx_fc, "y": hy_fc, "z": hz_fc}
+
+    div_h_cc = None
+    for d in noninvariant_dims:
+        other_dims = noninvariant_dims - {d}
+        # ideally, we'd have an extra index in the dth dimension of the dth component of h, but PSC doesn't write that
+        # so instead, cut off 1 index from the non-dth dimension of the dth component of h
+        hd_sliced = h_comps[d].isel({other_dim: slice(0, -1) for other_dim in other_dims})
+
+        # doing this diff then reduces the dth dimension by 1 index, so all deltas have the same dimension
+        delta_hd = hd_sliced.isel({d: slice(1, None)}).data - hd_sliced.isel({d: slice(0, -1)}).data
+        delta_d = float(coords[d][1] - coords[d][0])
+        diff_hd = delta_hd / delta_d
+
+        div_h_cc = diff_hd if div_h_cc is None else div_h_cc + diff_hd
+
+    new_coords = {d: (coord[:-1] if d in noninvariant_dims else coord) for d, coord in coords.items()}
+
+    return DataArray(div_h_cc, new_coords, dims=hx_fc.dims)
