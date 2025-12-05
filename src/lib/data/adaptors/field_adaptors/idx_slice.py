25e3c6616b1d448e9877f60d66d08df51d013236
James McClung <james@themcclungs.net>
2025-12-05 14:31:12 -0500

    idx_slice: put i in name frag

--- src/lib/data/adaptors/field_adaptors/idx_slice.py
+++ src/lib/data/adaptors/field_adaptors/idx_slice.py
@@ -18,7 +18,7 @@ class IdxSlice(AtomicAdaptor):
     def get_name_fragments(self) -> list[str]:
         lower = f"{self.lower_inclusive}" if self.lower_inclusive is not None else ""
         upper = f"{self.upper_exclusive}" if self.upper_exclusive is not None else ""
-        return [f"slice_{self.dim_name}={lower}:{upper}"]
+        return [f"islice_{self.dim_name}={lower}:{upper}"]
 
 
 IDX_SLICE_FORMAT = "dim_name=lower:upper"
