69f8b343155298eef229d44e3dfdc9773bb115d6
James McClung <james@themcclungs.net>
2025-12-15 16:43:03 -0500

    reduce: +ModifyVarLatex

--- src/lib/data/adaptors/field_adaptors/reduce.py
+++ src/lib/data/adaptors/field_adaptors/reduce.py
@@ -13,11 +13,27 @@ class ReduceFunc(typing.Protocol):
     def __call__(self, da: xr.DataArray, dim_name: str) -> xr.DataArray: ...
 
 
-REDUCE_FUNCS: dict[str, ReduceFunc] = {
-    "mean": lambda da, dim_name: da.reduce(np.nanmean, dim_name),
-    "integrate": lambda da, dim_name: da.integrate(dim_name),
-    "max": lambda da, dim_name: da.max(dim_name, skipna=True),
-    "min": lambda da, dim_name: da.min(dim_name, skipna=True),
+class ModifyVarLatex(typing.Protocol):
+    def __call__(self, var_latex: str, dim_name: str) -> str: ...
+
+
+REDUCE_FUNCS: dict[str, tuple[ReduceFunc, ModifyVarLatex]] = {
+    "mean": (
+        lambda da, dim_name: da.reduce(np.nanmean, dim_name),
+        lambda var_latex, dim_name: f"\\langle{var_latex}\\rangle_{{{dim_name}}}",
+    ),
+    "integrate": (
+        lambda da, dim_name: da.integrate(dim_name),
+        lambda var_latex, dim_name: f"\\int \\text{{d}}{dim_name}\\,({var_latex})",
+    ),
+    "max": (
+        lambda da, dim_name: da.max(dim_name, skipna=True),
+        lambda var_latex, dim_name: f"\\max_{{{dim_name}}}({var_latex})",
+    ),
+    "min": (
+        lambda da, dim_name: da.min(dim_name, skipna=True),
+        lambda var_latex, dim_name: f"\\min_{{{dim_name}}}({var_latex})",
+    ),
 }
 
 
@@ -27,7 +43,10 @@ class Reduce(AtomicAdaptor):
         self.func_name = func_name
 
     def apply_atomic(self, da: xr.DataArray) -> xr.DataArray:
-        return REDUCE_FUNCS[self.func_name](da, self.dim_name)
+        return REDUCE_FUNCS[self.func_name][0](da, self.dim_name)
+
+    def get_modified_var_latex(self, var_latex: str) -> str:
+        return REDUCE_FUNCS[self.func_name][1](var_latex, self.dim_name)
 
     def get_name_fragments(self) -> list[str]:
         return [f"reduce_{self.dim_name}={self.func_name}"]
