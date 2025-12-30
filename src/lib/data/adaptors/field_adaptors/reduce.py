75ad943fcd9b6bd3d46aa9b932c35593c4d4c066
James McClung <james@themcclungs.net>
2025-12-30 10:35:32 -0500

    reduce: accept multiple dim names

--- src/lib/data/adaptors/field_adaptors/reduce.py
+++ src/lib/data/adaptors/field_adaptors/reduce.py
@@ -11,46 +11,46 @@ from ..registry import adaptor_parser
 
 
 class ReduceFunc(typing.Protocol):
-    def __call__(self, da: xr.DataArray, dim_name: str) -> xr.DataArray: ...
+    def __call__(self, da: xr.DataArray, dim_names: list[str]) -> xr.DataArray: ...
 
 
 class ModifyVarLatex(typing.Protocol):
-    def __call__(self, var_latex: str, dim_name: str) -> str: ...
+    def __call__(self, var_latex: str, dim_names: list[str]) -> str: ...
 
 
 REDUCE_FUNCS: dict[str, tuple[ReduceFunc, ModifyVarLatex]] = {
     "mean": (
-        lambda da, dim_name: da.reduce(np.nanmean, dim_name),
-        lambda var_latex, dim_name: f"\\langle{var_latex}\\rangle_{{{dim_name}}}",
+        lambda da, dim_names: da.reduce(np.nanmean, dim_names),
+        lambda var_latex, dim_names: f"\\langle{var_latex}\\rangle_{{{','.join(dim_names)}}}",
     ),
     "integrate": (
-        lambda da, dim_name: da.integrate(dim_name),
-        lambda var_latex, dim_name: f"\\int \\text{{d}}{dim_name}\\,({var_latex})",
+        lambda da, dim_names: da.integrate(dim_names),
+        lambda var_latex, dim_names: f"\\int \\text{{d}}{'\\text{d}'.join(dim_names)}\\,({var_latex})",
     ),
     "max": (
-        lambda da, dim_name: da.max(dim_name, skipna=True),
-        lambda var_latex, dim_name: f"\\max_{{{dim_name}}}({var_latex})",
+        lambda da, dim_names: da.max(dim_names, skipna=True),
+        lambda var_latex, dim_names: f"\\max_{{{','.join(dim_names)}}}({var_latex})",
     ),
     "min": (
-        lambda da, dim_name: da.min(dim_name, skipna=True),
-        lambda var_latex, dim_name: f"\\min_{{{dim_name}}}({var_latex})",
+        lambda da, dim_names: da.min(dim_names, skipna=True),
+        lambda var_latex, dim_names: f"\\min_{{{','.join(dim_names)}}}({var_latex})",
     ),
 }
 
 
 class Reduce(BareAdaptor):
-    def __init__(self, dim_name: str, func_name: str):
-        self.dim_name = dim_name
+    def __init__(self, dim_names: str | list[str], func_name: str):
+        self.dim_names = [dim_names] if isinstance(dim_names, str) else dim_names
         self.func_name = func_name
 
     def apply_bare(self, da: xr.DataArray) -> xr.DataArray:
-        return REDUCE_FUNCS[self.func_name][0](da, self.dim_name)
+        return REDUCE_FUNCS[self.func_name][0](da, self.dim_names)
 
     def get_modified_var_latex(self, var_latex: str) -> str:
-        return REDUCE_FUNCS[self.func_name][1](var_latex, self.dim_name)
+        return REDUCE_FUNCS[self.func_name][1](var_latex, self.dim_names)
 
     def get_name_fragments(self) -> list[str]:
-        return [f"reduce_{self.dim_name}={self.func_name}"]
+        return [f"reduce_{','.join(self.dim_names)}={self.func_name}"]
 
 
 REDUCE_FORMAT = "dim_name=reduce_func"
