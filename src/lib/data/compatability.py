3fa7b217f90700764ed2b1193840be5521aca112
James McClung <james@themcclungs.net>
2026-01-09 17:14:14 -0500

    compatability: mv out messages

--- src/lib/data/compatability.py
+++ src/lib/data/compatability.py
@@ -8,7 +8,8 @@ class DataError(Exception): ...
 def ensure_type[Data](consumer_name: str, data: Data, *allowed_types: type):
     if not isinstance(data, allowed_types):
         names_of_types = ", ".join(f"{t.__module__}.{t.__name__}" for t in allowed_types)
-        raise DataError(f"{consumer_name} accepts only the following types: [{names_of_types}], but received data of type {data.__class__.__module__}.{data.__class__.__name__}")
+        message = f"{consumer_name} accepts only the following types: [{names_of_types}], but received data of type {data.__class__.__module__}.{data.__class__.__name__}"
+        raise DataError(message)
 
 
 def get_allowed_types(type_annotation) -> list[type]:
@@ -19,4 +20,5 @@ def get_allowed_types(type_annotation) -> list[type]:
     elif isinstance(type_annotation, typing.TypeVar):
         return get_allowed_types(type_annotation.__bound__)
     else:
-        raise NotImplementedError(f"not sure how to find allowed types for the following type annotation: {type_annotation}")
+        message = f"not sure how to find allowed types for the following type annotation: {type_annotation}"
+        raise NotImplementedError(message)
