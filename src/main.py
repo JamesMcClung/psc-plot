1e8ad7188f429ed48c88a37855c1e2702dc37323
James McClung <james@themcclungs.net>
2026-03-19 15:55:16 -0400

    main: respect ffmpeg_bin

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import dask
+import matplotlib.pyplot as plt
 
 from lib import parsing
+from lib.config import CONFIG
 
 dask.config.set(num_workers=1)
 
@@ -11,5 +13,7 @@ anim = args.get_animation()
 if args.show:
     anim.show()
 if args.save is not None:
+    if CONFIG.ffmpeg_bin:
+        plt.rcParams["animation.ffmpeg_path"] = CONFIG.ffmpeg_bin
     args.save.mkdir(exist_ok=True)
     anim.save(args.save)
