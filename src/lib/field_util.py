0e275957ce3a1c2c525f29f69d65a4dd9788e099
James McClung <james@themcclungs.net>
2026-03-19 09:56:28 -0400

    field_util: -load_ds

--- src/lib/field_util.py
+++ src/lib/field_util.py
@@ -1,8 +1,5 @@
 from pathlib import Path
 
-import pscpy
-import xarray
-
 from . import file_util
 
 
@@ -12,11 +9,3 @@ def get_available_field_steps(prefix: file_util.FieldPrefix) -> list[int]:
 
 def get_path(prefix: file_util.FieldPrefix, step: int) -> Path:
     return file_util.ROOT_DIR / f"{prefix}.{step:09}.bp"
-
-
-def load_ds(prefix: file_util.FieldPrefix, step: int) -> xarray.Dataset:
-    data_path = get_path(prefix, step)
-    ds = xarray.load_dataset(data_path)
-    # FIXME don't hardcode species names
-    ds = pscpy.decode_psc(ds, ["e", "i"])
-    return ds
