287ffc5b995e3c778974be32ab592e4904131e87
James McClung <james@themcclungs.net>
2026-03-26 07:22:12 -0400

    derive: check var_key

--- src/lib/data/adaptors/derive.py
+++ src/lib/data/adaptors/derive.py
@@ -3,8 +3,10 @@ import dask.dataframe as dd
 from lib.data.adaptor import CheckedAdaptor
 from lib.data.data_with_attrs import List
 from lib.derived_particle_variables.derived_particle_variable import (
+    DERIVED_PARTICLE_VARIABLES,
     derive_particle_variable,
 )
+from lib.parsing import parse_util
 from lib.parsing.args_registry import arg_parser
 
 
@@ -27,4 +29,5 @@ _DERIVE_FORMAT = "var_key"
 )
 def parse_derive(arg: str) -> Derive:
     var_key = arg
+    parse_util.check_value(var_key, "var_key", DERIVED_PARTICLE_VARIABLES["prt"])
     return Derive(var_key)
