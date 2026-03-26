fc2de0761f43c75d81ee626956249d407e279a80
James McClung <james@themcclungs.net>
2026-03-26 16:12:36 -0400

    roll: nargs=+

--- src/lib/data/adaptors/roll.py
+++ src/lib/data/adaptors/roll.py
@@ -7,15 +7,15 @@ from lib.parsing.args_registry import arg_parser
 
 
 class Roll(BareAdaptor):
-    def __init__(self, dim_name: str, roll_window: int):
-        self.dim_name = dim_name
-        self.window_size = roll_window
+    def __init__(self, dim_names_to_window_size: dict[str, int]):
+        self.dim_names_to_window_size = dim_names_to_window_size
 
     def apply_bare(self, da: xr.DataArray) -> xr.DataArray:
-        return da.rolling({self.dim_name: self.window_size}).mean()
+        return da.rolling(self.dim_names_to_window_size).mean()
 
     def get_name_fragments(self) -> list[str]:
-        return [f"roll_{self.dim_name}={self.window_size}"]
+        subfrags = "_".join(f"{dim_name}={bin_size}" for dim_name, bin_size in self.dim_names_to_window_size.items())
+        return [f"roll_{subfrags}"]
 
 
 ROLL_FORMAT = "dim_name=window_size"
@@ -26,12 +26,16 @@ ROLL_FORMAT = "dim_name=window_size"
     flags="--roll",
     metavar=ROLL_FORMAT,
     help="plot the rolling average against the given dimension with the given window size",
+    nargs="+",
 )
-def parse(arg: str) -> Roll:
-    [dim_name, window_size_arg] = parse_util.parse_assignment(arg, ROLL_FORMAT)
+def parse(args: list[str]) -> Roll:
+    dim_names_to_window_size = {}
+    for arg in args:
+        [dim_name, window_size_arg] = parse_util.parse_assignment(arg, ROLL_FORMAT)
 
-    parse_util.check_value(dim_name, "dim_name", DIMENSIONS)
+        parse_util.check_value(dim_name, "dim_name", DIMENSIONS)
+        window_size = parse_util.parse_number(window_size_arg, "window_size", int)
 
-    window_size = parse_util.parse_number(window_size_arg, "window_size", int)
+        dim_names_to_window_size[dim_name] = window_size
 
-    return Roll(dim_name, window_size)
+    return Roll(dim_names_to_window_size)
