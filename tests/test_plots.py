0a5dc6e7a66770a2093f79fb279304fcbb821f8d
James McClung <james@themcclungs.net>
2026-04-13 12:35:05 -0400

    test_plots: update with practical cases and comments

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -4,69 +4,151 @@ from conftest import make_plot
 MPL_KWARGS = dict(tolerance=2, savefig_kwarg={"dpi": 100}, style="default")
 
 
-# --- Static 1D field ---
+# --- Fields ---
+
+
+@pytest.mark.mpl_image_compare(**MPL_KWARGS)
+def test_animated_1d():
+    """X-component of magnetic field, implicitly averaged across z."""
+    return make_plot("pfd hx_fc -v y".split())
+
+
+@pytest.mark.mpl_image_compare(**MPL_KWARGS)
+def test_static_1d():
+    """X-component of magnetic field at the last time step, implicitly averaged across z. Note video is disabled via `time=`."""
+    return make_plot("pfd hx_fc -i t=-1 -v y time=".split())
+
+
+@pytest.mark.mpl_image_compare(**MPL_KWARGS)
+def test_animated_2d():
+    """2D view of x-component of magnetic field."""
+    return make_plot("pfd hx_fc -v y z".split())
+
+
+@pytest.mark.mpl_image_compare(**MPL_KWARGS)
+def test_animated_2d_derived():
+    """2D view of magnetic field square-amplitude. Since each component of H is centered differently, and `--derive` doesn't currently support recentering, use the builtin derived field `h2_cc`."""
+    return make_plot("pfd h2_cc -v y z".split())
+
+
+@pytest.mark.mpl_image_compare(**MPL_KWARGS)
+def test_animated_2d_idx():
+    """2D slice of x-component of magnetic field at the x=1 index."""
+    return make_plot("pfd hx_fc -i x=1 -v y z".split(), data_dir="test-3d")
+
+
+@pytest.mark.mpl_image_compare(**MPL_KWARGS)
+def test_animated_2d_pos():
+    """2D slice of x-component of magnetic field nearest to y=0.06."""
+    return make_plot("pfd hx_fc --pos y=0.06 -v x z".split(), data_dir="test-3d")
+
+
+@pytest.mark.mpl_image_compare(**MPL_KWARGS)
+def test_animated_1d_downsample():
+    """Downsample (i.e., coarsen) by a factor of 2."""
+    return make_plot("pfd hx_fc --downsample y=2 -v y".split())
+
+
+@pytest.mark.mpl_image_compare(**MPL_KWARGS)
+def test_animated_1d_bin():
+    """Downsample via binning, specifying the final dimension size rather than the downsample factor."""
+    return make_plot("pfd hx_fc --bin y=8 -v y".split())
+
+
+@pytest.mark.mpl_image_compare(**MPL_KWARGS)
+def test_animated_1d_rolling():
+    """Smooth data via a rolling average."""
+    return make_plot("pfd hx_fc --roll y=3 -v y".split())
+
+
+# --- Turbulence power spectrum ---
+
+
+@pytest.mark.mpl_image_compare(**MPL_KWARGS)
+def test_spectrum_1d():
+    """1D power spectrum of the x-component of the magnetic field. Note `--pos` and `-i` to remove negative and 0-mode, respectively. The `--mul 2` recovers the (symmetric) power of negative modes."""
+    return make_plot("pfd hx_fc -f y z --mag --pow 2 -v k_y --pos k_y=0: -i k_y=1: --mul 2 --scale log k_y=log".split())
+
+
+@pytest.mark.mpl_image_compare(**MPL_KWARGS)
+def test_spectrum_3d():
+    """Total power spectrum of the magnetic field. The builtin `hhat2` is the sum of the squares of the Fourier-transformed copmonents of the magnetic field. The combination of `--scatter` and `--transform-spherical` avoids interpolating onto a grid when doing the coordinate transformation. Note `--pos` removes the 0-mode. This is the current intended use case for `--fit`, which fits a power law index, but also necessitates `--compute` due to a bug."""
+    return make_plot("pfd hhat2 --scatter --transform-spherical k_y k_z k_x --pos k_s=1e-8: -v k_s hhat2 --scale hhat2=log --compute --fit 25:45".split(), data_dir="test-3d")
+
+
+# --- Particles ---
+
+
+@pytest.mark.mpl_image_compare(**MPL_KWARGS)
+def test_animated_scatter_ion_positions():
+    """A real-space scatter plot of electron positions, with cell edges shown via `--grid`. Grid line positions aren't coordinate-informed, so the spacing is manually specified."""
+    return make_plot("prt --species electron -v y z --grid y=0.0625 z=0.0625".split(), data_dir="test-3d")
+
+
+@pytest.mark.mpl_image_compare(**MPL_KWARGS)
+def test_animated_scatter_ion_phase():
+    """A phase-space scatter plot of ions. Color is used to show pz, but requires `--compute` due to a bug."""
+    return make_plot("prt --species ion -v y py color=pz --compute".split())
+
+
+@pytest.mark.mpl_image_compare(**MPL_KWARGS)
+def test_animated_2d_binned_phase():
+    """A clearer way to see phase space distributions for many particles. Since y is a dimension with coordinates, those coordinates are automatically used as bin edges. The combination of `--nan0` and `--scale log` makes faint phase-space structures very easy to see."""
+    return make_plot("prt --species ion --bin y py=16 -v y py --nan0 --scale log".split())
+
+
+# --- Particle moments ---
 
 
 @pytest.mark.mpl_image_compare(**MPL_KWARGS)
 def test_static_1d_rho_i():
-    return make_plot(["pfd_moments", "rho_i", "-v", "y", "time="])
-
-
-# --- Animated 1D field ---
-
-
-@pytest.mark.mpl_image_compare(**MPL_KWARGS)
-def test_animated_1d_hx():
-    return make_plot(["pfd", "hx_fc", "-v", "y"])
-
-
-# --- Animated 2D field ---
+    """A typical plot of initial ion charge density, implicitly averaged across z."""
+    return make_plot("pfd_moments rho_i -v y time=".split())
 
 
 @pytest.mark.mpl_image_compare(**MPL_KWARGS)
-def test_animated_2d_hx():
-    return make_plot(["pfd", "hx_fc", "-v", "y", "z"])
+def test_static_1d_rho_derive():
+    """Use --derive to get the total charge density, like so. Note that some initial field must be set (rho_i), but the result of --derive is what ends up being plotted."""
+    return make_plot("pfd_moments rho_i --derive rho=rho_i+rho_e -v y time=".split())
 
 
 @pytest.mark.mpl_image_compare(**MPL_KWARGS)
-def test_animated_2d_hx_3d():
-    return make_plot(["pfd", "hx_fc", "-v", "y", "z"], data_dir="test-3d")
+def test_static_1d_rho():
+    """Total charge density is also (currently) a predefined deriveable variable."""
+    return make_plot("pfd_moments rho -v y time=".split())
 
 
-# --- Animated scatter ---
-
-
-@pytest.mark.mpl_image_compare(**MPL_KWARGS)
-def test_animated_scatter_ion():
-    return make_plot(["prt", "--species", "ion", "-v", "y", "py"])
-
-
-# --- Hooks on static 1D ---
+# --- Gauss error ---
 
 
 @pytest.mark.mpl_image_compare(**MPL_KWARGS)
-def test_static_1d_with_grid():
-    return make_plot(["pfd_moments", "rho_i", "-v", "y", "time=", "--grid", "y"])
+def test_gauss_spatial():
+    """Maximum absolute error in Gauss' law. Note `mag` (magnitude, or absolute value) before `--reduce`. As usual, `--nan0` and `--scale log` play well together."""
+    return make_plot("gauss error --mag --reduce x,z=max -v y --nan0 --scale log".split(), data_dir="test-3d")
 
 
 @pytest.mark.mpl_image_compare(**MPL_KWARGS)
-def test_static_1d_with_vline():
-    return make_plot(["pfd_moments", "rho_i", "-v", "y", "time=", "--vline", "0.5=half"])
+def test_gauss_temporal():
+    """Maximum absolte error in Gauss' law as a function of time. Note `time=` in `-v` to disable the default behavior of `time=t`."""
+    return make_plot("gauss error --mag --reduce x,y,z=max -v t time= --nan0 --scale log".split(), data_dir="test-3d")
+
+
+# --- Misc ---
 
 
 @pytest.mark.mpl_image_compare(**MPL_KWARGS)
-def test_static_1d_with_scale():
-    return make_plot(["pfd_moments", "rho_i", "-v", "y", "time=", "--scale", "symlog"])
-
-
-# --- Hooks on animated ---
-
-
-@pytest.mark.mpl_image_compare(**MPL_KWARGS)
-def test_animated_1d_with_scale():
-    return make_plot(["pfd", "hx_fc", "-v", "y", "--scale", "symlog"])
+def test_vline():
+    """`--vline` supports a labeled and unlabeled version."""
+    return make_plot("prt -v y z --vline 0.15=label 0.03".split())
 
 
 @pytest.mark.mpl_image_compare(**MPL_KWARGS)
-def test_animated_2d_with_scale():
-    return make_plot(["pfd", "hx_fc", "-v", "y", "z", "--scale", "symlog"])
+def test_scale_symlog():
+    """Symlog scale with specific linear threshold."""
+    return make_plot("pfd hx_fc -v y z --scale symlog#.01".split())
+
+
+@pytest.mark.mpl_image_compare(**MPL_KWARGS)
+def test_polar_grid():
+    """Polar plot."""
+    return make_plot("pfd hx_fc --transform-polar y z -v r_p theta".split())
