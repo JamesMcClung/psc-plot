801ceb57a2ce4bb6ac35ef46f0f8483653dc4d9d
James McClung <james@themcclungs.net>
2026-04-18 09:51:17 -0400

    cli: better handling of image formats

--- src/lib/cli.py
+++ src/lib/cli.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 from lib import parsing
 from lib.config import CONFIG
 from lib.parsing.args import Args
-from lib.plotting.animated_plot import AnimatedPlot
 from lib.plotting.plot import SaveFormat
 
 
@@ -31,7 +30,8 @@ def _resolve_save_format(args: Args) -> SaveFormat | None:
     if CONFIG.ffmpeg_bin:
         return "mp4"
 
-    warnings.warn("ffmpeg not found; saving as gif instead of mp4")
+    message = "ffmpeg not found; will save animations as gif instead of mp4"
+    warnings.warn(message)
     return "gif"
 
 
@@ -39,6 +39,7 @@ def main():
     dask.config.set(num_workers=CONFIG.dask_num_workers)
 
     args = parsing.get_parsed_args()
+    # resolve format BEFORE applying pipeline in order to fail early
     format = _resolve_save_format(args)
 
     if format == "mp4":
@@ -50,5 +51,14 @@ def main():
         plot.show()
     if args.save is not None:
         args.save.mkdir(exist_ok=True)
-        format = format if isinstance(plot, AnimatedPlot) else None
+
+        if format not in plot.allowed_save_formats():
+            if format == args.save_format:  # user actually specified this format
+                message = f"{format} is incompatible with the data; reverting to default ({plot.default_save_format()})"
+                warnings.warn(message)
+            else:
+                assert args.save_format is None
+
+            format = plot.default_save_format()
+
         plot.save(args.save, format=format)
