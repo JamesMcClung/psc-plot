5cb7f6ce45cffe6412a398dd69bf55d61c595966
James McClung <james@themcclungs.net>
2026-05-21 15:22:55 -0400

    test_save_filename: +scale test [fails]

--- tests/test_save_filename.py
+++ tests/test_save_filename.py
@@ -16,6 +16,7 @@ def _stem(args_list: list[str]) -> str:
         (["pfd", "hx_fc"], "pfd-hx_fc-vs_y,z;time=t"),
         (["pfd", "hx_fc", "--nan0"], "pfd-hx_fc-nan0-vs_y,z;time=t"),
         (["pfd", "hx_fc", "--grid", "x"], "pfd-hx_fc-vs_y,z;time=t"),
+        (["pfd", "hx_fc", "--scale", "log"], "pfd-hx_fc-vs_y,z;time=t-scale_log"),
     ],
 )
 def test_save_file_stem(args_list, expected_stem):
