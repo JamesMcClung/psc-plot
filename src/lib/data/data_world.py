acf4a965561af0c00234566e8ce03cc165d763a2
James McClung <james@themcclungs.net>
2026-07-14 16:27:46 -0400

    data_world: +active_data

--- src/lib/data/data_world.py
+++ src/lib/data/data_world.py
@@ -5,3 +5,9 @@ class DataWorld:
     def __init__(self):
         self.datas: dict[str, DataWithAttrs] = {}
         self.active_key: str | None = None
+
+    @property
+    def active_data(self) -> DataWithAttrs | None:
+        if self.active_key is None:
+            return None
+        return self.datas[self.active_key]
