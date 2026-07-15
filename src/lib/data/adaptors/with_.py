064ac050114e276778ee7940e2b917f484a2e219
James McClung <james@themcclungs.net>
2026-07-15 14:25:07 -0400

    with: ->prefix_or_key

--- src/lib/data/adaptors/with_.py
+++ src/lib/data/adaptors/with_.py
@@ -1,38 +1,39 @@
 from lib.config import CONFIG
-from lib.data.adaptor import Adaptor
-from lib.data.data_with_attrs import DataWithAttrs
+from lib.data.adaptor import WorldAdaptor
 from lib.data.loader import discover_loaders
 from lib.parsing import parse_util
 from lib.parsing.args_registry import arg_parser
 
 
-class With(Adaptor):
-    def __init__(self, prefix: str | None, key: str | None):
-        self.prefix = prefix
+class With(WorldAdaptor):
+    def __init__(self, prefix_or_key: str, key: str | None = None):
+        self.prefix_or_key = prefix_or_key
         self.key = key
 
     def apply_world(self, world):
-        if self.prefix:
-            if self.prefix not in world.datas:
-                loaders = discover_loaders(CONFIG.data_dir)
-                data = loaders[self.prefix](self.prefix, self.key).get_data()
-            else:
-                data = world.active_data
+        # case 1: prefix_or_key is a key within the active prefix
+        if not self.key and world.active_key and self.prefix_or_key in world.active_data.metadata.var_infos:
+            key = self.prefix_or_key
+            return world.with_active_data(world.active_data.assign_metadata(active_key=key))
 
-            world = world.with_active_data(data, active_key=self.prefix)
+        # case 2: prefix_or_key is a prefix
+        prefix = self.prefix_or_key
+        key = self.key
 
-        return super().apply_world(world)
+        if prefix in world.datas:
+            return world.with_active_data(world.active_data.assign_metadata(active_key=key), prefix)
 
-    def apply(self, data: DataWithAttrs) -> DataWithAttrs:
-        return data.assign_metadata(active_key=self.key)
+        loader_types = discover_loaders(CONFIG.data_dir)
+        loader = loader_types[prefix](prefix, key)
+        return loader.apply_world(world)
 
     def get_name_fragments(self) -> list[str]:
-        maybe_prefix = f"{self.prefix}{SCOPE_OP}" if self.prefix else ""
+        maybe_prefix = f"{self.prefix_or_key}{SCOPE_OP}" if self.prefix_or_key else ""
         return [f"with_{maybe_prefix}{self.key or ''}"]
 
 
 SCOPE_OP = "::"
-WITH_FORMAT = f"prefix{SCOPE_OP}[key] | key"
+WITH_FORMAT = f"prefix[{SCOPE_OP}[key]] | key"
 
 
 @arg_parser(
@@ -50,9 +51,7 @@ def parse_with(arg: str) -> With:
         key = parse_util.parse_optional_identifier(split_arg[1] or None, "key")
         return With(prefix, key)
     elif len(split_arg) == 1:
-        prefix = None
-        key = parse_util.parse_identifier(split_arg[0], "key")
+        prefix_or_key = parse_util.parse_identifier(split_arg[0], "prefix | key")
+        return With(prefix_or_key)
     else:
         parse_util.fail_format(arg, WITH_FORMAT)
-
-    return With(prefix, key)
