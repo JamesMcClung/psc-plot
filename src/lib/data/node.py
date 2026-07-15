1af1786967c273a186907bfd2f32539d449149e5
James McClung <james@themcclungs.net>
2026-07-15 08:10:58 -0400

    node: +PlotNode

--- src/lib/data/node.py
+++ src/lib/data/node.py
@@ -4,6 +4,9 @@ from functools import cache
 from lib.data.adaptor import Adaptor
 from lib.data.data_world import DataWorld
 from lib.data.loader import Loader
+from lib.plotting.get_plot import get_plot
+from lib.plotting.hook import Hook
+from lib.plotting.plot import Plot
 
 
 class DataProcessingNode[D](ABC):
@@ -33,3 +36,20 @@ class LoaderNode(DataProcessingNode[DataWorld]):
     @cache
     def pull(self) -> DataWorld:
         return DataWorld(self.loader.get_data(), self.loader.active_key)
+
+
+class PlotNode(DataProcessingNode[Plot]):
+    def __init__(self, input_node: DataProcessingNode[DataWorld], hooks: list[Hook]):
+        super().__init__(input_node.name_fragments + [frag for hook in hooks for frag in hook.get_name_fragments()])
+        self.input_node = input_node
+        self.hooks = hooks
+
+    @cache
+    def pull(self) -> Plot:
+        data = self.input_node.pull()
+        plot = get_plot(data.active_data)
+
+        for hook in self.hooks:
+            plot.add_hook(hook)
+
+        return plot
