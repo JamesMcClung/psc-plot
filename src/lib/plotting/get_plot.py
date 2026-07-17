1711698a90a27846e4bd26aa51c9915521ee0d83
James McClung <james@themcclungs.net>
2026-07-17 11:29:38 -0400

    get_plot: -old get_renderer

--- src/lib/plotting/get_plot.py
+++ src/lib/plotting/get_plot.py
@@ -1,4 +1,4 @@
-from lib.data.data_with_attrs import DataWithAttrs, Field, List
+from lib.data.data_with_attrs import Field, List
 from lib.data.data_world import DataWorld
 from lib.data.plot_target import SpatialDimsRTheta, SpatialDimsXY
 from lib.plotting.animated_plot import AnimatedPlot
@@ -9,28 +9,6 @@ from lib.plotting.renderers.field_2d import Field2dRenderer
 from lib.plotting.renderers.polar_field import PolarFieldRenderer
 from lib.plotting.renderers.scatter import ScatterRenderer
 from lib.plotting.static_plot import StaticPlot
-
-
-def _get_renderer(data: DataWithAttrs) -> Renderer:
-    spatial_dims = data.metadata.spatial_dims
-
-    if isinstance(data, Field):
-        if len(spatial_dims) == 1:
-            return Field1dRenderer()
-        elif len(spatial_dims) == 2:
-            if data.metadata.var_infos[spatial_dims[0]].geometry == "polar:r" and data.metadata.var_infos[spatial_dims[1]].geometry == "polar:theta":
-                return PolarFieldRenderer()
-            return Field2dRenderer()
-        else:
-            raise NotImplementedError("don't have 3D field plots yet")
-
-    elif isinstance(data, List):
-        if len(spatial_dims) == 2:
-            return ScatterRenderer()
-        else:
-            raise NotImplementedError(f"don't have {len(spatial_dims)}D scatter plots yet")
-
-    raise TypeError(f"unexpected data type: {type(data)}")
 
 
 def get_plot(world: DataWorld) -> Plot:
