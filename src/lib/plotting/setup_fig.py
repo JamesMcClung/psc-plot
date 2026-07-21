f35cf1b04d76d19da8f07cd913d9e577a6497d94
James McClung <james@themcclungs.net>
2026-07-21 13:19:54 -0400

    setup_fig: +AxesManagerMultiLine

--- src/lib/plotting/setup_fig.py
+++ src/lib/plotting/setup_fig.py
@@ -5,11 +5,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
+from matplotlib.lines import Line2D
 from matplotlib.projections import PolarAxes
 from matplotlib.text import Text
 
 from lib.plotting import plt_util
-from lib.plotting.plot_info import ImageInfo, LineInfo, PlotInfo, PlotInfo2D, PolarMeshInfo, ScatterInfo
+from lib.plotting.plot_info import DimKey, ImageInfo, LineInfo, PlotInfo, PlotInfo2D, PolarMeshInfo, ScatterInfo
 
 type AxesIdx = tuple[int, int]
 
@@ -221,6 +222,111 @@ class AxesManagerSinglePolarMesh(AxesManagerSingle[PolarAxes, PolarMeshInfo]):
         plt_util.update_cbar(image, data_min_override=data_lower, data_max_override=data_upper)
 
 
+class AxesManagerMultiLine(AxesManager):
+    def __init__(self, ax: Axes, infos: list[LineInfo]):
+        self.ax = ax
+        self.infos = infos
+
+        self.common_coord_dims: set[DimKey] = set()
+        self.unique_coord_dimss: list[set[DimKey]] = [set() for _ in self.infos]
+        self._update_common_scalar_coordinates()
+
+        self.lines: list[Line2D] = []  # populated later
+
+    def _update_common_scalar_coordinates(self):
+        self.common_coord_dims.clear()
+        for unique_dims in self.unique_coord_dimss:
+            unique_dims.clear()
+
+        all_dims: set[DimKey] = {dim for info in self.infos for dim in info.scalar_coord_values}
+
+        for dim in all_dims:
+            if all(dim in info.scalar_coord_values for info in self.infos) and len({info.get_coord_label(dim) for info in self.infos}) == 1:
+                self.common_coord_dims.add(dim)
+            else:
+                for info, unique_dims in zip(self.infos, self.unique_coord_dimss):
+                    if dim in info.scalar_coord_values:
+                        unique_dims.add(dim)
+
+    def _get_title(self) -> str:
+        return ", ".join(self.infos[0].get_coord_label(dim).maybe_with_dollars() for dim in self.common_coord_dims)
+
+    def _get_legend_labels(self) -> list[str]:
+        legend_labels: list[str] = []
+
+        for info, unique_dims in zip(self.infos, self.unique_coord_dimss):
+            coord_labels_str = ", ".join(info.get_coord_label(dim).maybe_with_dollars() for dim in unique_dims)
+
+            if info.subject and coord_labels_str:
+                legend_labels.append(f"{info.subject} ({coord_labels_str})")
+            else:
+                legend_labels.append(info.subject or coord_labels_str)
+
+        return legend_labels
+
+    def _update_title_and_legend(self, *_):
+        self._update_common_scalar_coordinates()
+        self.ax.set_title(self._get_title())
+        for line, label in zip(self.lines, self._get_legend_labels()):
+            line.set_label(label)
+
+    def setup(self):
+        self.setup_title()
+        self.setup_labels()
+        self.setup_data()
+        self.setup_scales()
+        self.setup_bounds()
+
+        for info in self.infos:
+            info._setter_callbacks["scalar_coord_values"] = self._update_title_and_legend
+
+    def setup_title(self):
+        self.ax.set_title(self._get_title())
+
+    def setup_labels(self):
+        x_labels = [info.get_dim_label(info.x_dim) for info in self.infos]
+        if (x_label := _one_or_none(x_labels)) is not None:
+            self.ax.set_xlabel(x_label)
+        else:
+            raise NotImplementedError(f"x labels must all be the same, but found {x_labels}")
+
+        y_labels = [info.get_dim_label(info.y_dim) for info in self.infos]
+        y_units = [info.dim_units[info.y_dim] for info in self.infos]
+        if (y_label := _one_or_none(y_labels)) is not None:
+            self.ax.set_ylabel(y_label)
+        elif (y_unit := _one_or_none(y_units)) is not None:
+            self.ax.set_ylabel(y_unit.maybe_with_dollars())
+        else:
+            raise NotImplementedError(f"y labels must all be the same unit, but found {y_units}")
+
+    def setup_scales(self):
+        x_scales = [info.dim_scales[info.x_dim] for info in self.infos]
+        if (x_scale := _one_or_none(x_scales)) is not None:
+            self.ax.set_xscale(x_scale.to_axis_scale())
+        else:
+            raise NotImplementedError(f"x scales must all be the same, but found {x_scales}")
+
+        y_scales = [info.dim_scales[info.y_dim] for info in self.infos]
+        if (y_scale := _one_or_none(y_scales)) is not None:
+            self.ax.set_yscale(y_scale.to_axis_scale())
+        else:
+            raise NotImplementedError(f"y scales must all be the same, but found {x_scales}")
+
+    def setup_bounds(self):
+        self.ax.set_xbound(*find_widest_bounds(info.dim_bounds[info.x_dim] for info in self.infos))
+        self.ax.set_ybound(*find_widest_bounds(info.dim_bounds[info.y_dim] for info in self.infos))
+
+    def setup_data(self):
+        for info, label in zip(self.infos, self._get_legend_labels()):
+            [line] = self.ax.plot(info.x_data, info.y_data, linestyle=info.line_style, scalex=False, scaley=False, label=label)
+            info._setter_callbacks["x_data"] = line.set_xdata
+            info._setter_callbacks["y_data"] = line.set_ydata
+            info._setter_callbacks["line_style"] = line.set_linestyle
+            self.lines.append(line)
+
+        self.ax.legend()
+
+
 def setup_fig(plot_infos: list[PlotInfo]) -> Figure:
     figure = plt.figure()
 
@@ -238,8 +344,10 @@ def setup_fig(plot_infos: list[PlotInfo]) -> Figure:
                 manager = AxesManagerSinglePolarMesh(ax, info)
             else:
                 raise TypeError(f"unknown type: {infos.__class__!r}")
+        elif all(isinstance(info, LineInfo) for info in infos):
+            manager = AxesManagerMultiLine(ax, infos)
         else:
-            raise NotImplementedError("don't yet support multiple plots per axes")
+            raise NotImplementedError("don't yet support multiple non-line plots per axes")
 
         manager.setup()
 
