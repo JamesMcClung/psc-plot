3976790b717772444948a956da5edf4d4bffd398
James McClung <james@themcclungs.net>
2026-07-27 09:10:57 -0400

    file_util: +Prepath

--- src/lib/file_util.py
+++ src/lib/file_util.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
+type Prepath = str
+"""Path from data root directory to the data-containing directory plus the data prefix, e.g. `"run5/pfd"`."""
+
 
 def get_available_steps(data_dir: Path, before_step: str, after_step: str) -> list[int]:
     files = data_dir.glob(f"{before_step}*{after_step}")
@@ -10,3 +13,10 @@ def get_available_steps(data_dir: Path, before_step: str, after_step: str) -> li
 
     steps.sort()
     return steps
+
+
+def split_prepath(prepath: Prepath) -> tuple[Path, str]:
+    components = prepath.rsplit("/", maxsplit=1)
+    if len(components) == 1:
+        return (Path("."), components[0])
+    return Path(components[0]), components[1]
