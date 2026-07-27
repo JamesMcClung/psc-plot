3d5bfce97d25ee6a1b306818109e1453646e9c72
James McClung <james@themcclungs.net>
2026-07-27 14:53:59 -0400

    versus: don't autoFourier; simplify

--- src/lib/data/adaptors/versus.py
+++ src/lib/data/adaptors/versus.py
@@ -2,7 +2,6 @@ from dataclasses import replace
 from typing import Literal
 
 from lib.data.adaptor import MetadataAdaptor
-from lib.data.adaptors.fourier import Fourier
 from lib.data.adaptors.reduce import Reduce
 from lib.data.data_with_attrs import DataWithAttrs, Field, List
 from lib.data.plot_target import PlotTarget, SpatialDims, SpatialDimsRTheta, SpatialDimsXY
@@ -24,19 +23,19 @@ class Versus(MetadataAdaptor):
         self.color_dim = color_dim
         self.axes_idx = axes_idx
 
-    def _get_retained_dim_keys(self, data: DataWithAttrs) -> list[str]:
-        retained_dims = self.spatial_dims.copy()
+    def _get_unused_dims(self, data: DataWithAttrs) -> list[str]:
+        unused_dims = data.dims.copy()
+
+        for space_dim in self.spatial_dims:
+            unused_dims.remove(space_dim)
 
         if time_dim := self._get_time_dim(data):
-            retained_dims.append(time_dim)
+            unused_dims.remove(time_dim)
 
         if self.color_dim:
-            retained_dims.append(self.color_dim)
+            unused_dims.remove(self.color_dim)
 
-        if isinstance(data, List) and data.metadata.active_key:
-            retained_dims.append(data.metadata.active_key)
-
-        return retained_dims
+        return unused_dims
 
     def _get_time_dim(self, data: DataWithAttrs) -> str | None:
         if self.time_dim_rule != "guess":
@@ -85,40 +84,13 @@ class Versus(MetadataAdaptor):
         )
 
     def apply_field(self, data: Field) -> Field:
-        # 1. apply implicit coordinate transforms, as necessary
-        retained_dims = self._get_retained_dim_keys(data)
-        for dim_name in retained_dims:
-            # 1a. already have the coordinate; do nothing
-            if dim_name in data.dims:
-                continue
-
-            # 1b. need to do a Fourier transform
-            dim = data.metadata.var_infos[dim_name]
-            f_dim = dim.toggle_fourier()
-            if f_dim.key in data.dims:
-                data = data.with_info(f_dim.key, f_dim)
-                fourier = Fourier(f_dim.key)
-                data = fourier.apply(data)
-                continue
-
-            # 1c. need to do a coordinate transform
-            # TODO
-
-        # 2. reduce remaining dimensions via arithmetic mean
-        reduce_dims = [dim for dim in data.dims if dim not in retained_dims]
-        reduce = Reduce(reduce_dims, "mean")
+        reduce = Reduce(self._get_unused_dims(data), "mean")
         data = reduce.apply(data)
 
         return data
 
     def apply_list(self, data: List) -> List:
-        # 1. coordinate transform
-        # TODO
-
-        # 2. drop unused vars
-        keep_vars = self._get_retained_dim_keys(data)
-        drop_vars = [active_key for active_key in data.dims if active_key not in keep_vars]
-        data = data.assign(data.data.drop(columns=drop_vars))
+        data = data.assign(data.data.drop(columns=self._get_unused_dims(data)))
 
         spatial_dims = self.spatial_dims.copy()
         if len(spatial_dims) == 1 and data.metadata.active_key is not None and data.metadata.active_key not in spatial_dims:
