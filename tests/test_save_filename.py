474f4e1a140c9062f3efaa9dd2a0a75e80360668
James McClung <james@themcclungs.net>
2026-08-06 15:48:59 -0400

    test_save_filename: update with _

--- tests/test_save_filename.py
+++ tests/test_save_filename.py
@@ -8,10 +8,10 @@ from lib.parsing.parse import parse_args
 @pytest.mark.parametrize(
     "args_list, expected_stem",
     [
-        (["pfd", "hx_fc"], "pfd::hx_fc-v_y,z"),
-        (["pfd", "hx_fc", "--nan0"], "pfd::hx_fc-nan0-v_y,z"),
-        (["pfd", "hx_fc", "--scale", "log"], "pfd::hx_fc-scale_log-v_y,z"),
-        (["pfd", "hx_fc", "-v", "y", "z", "time="], "pfd::hx_fc-v_y,z;time="),
+        (["pfd", "hx_fc"], "pfd__hx_fc-v_y,z"),
+        (["pfd", "hx_fc", "--nan0"], "pfd__hx_fc-nan0-v_y,z"),
+        (["pfd", "hx_fc", "--scale", "log"], "pfd__hx_fc-scale_log-v_y,z"),
+        (["pfd", "hx_fc", "-v", "y", "z", "time="], "pfd__hx_fc-v_y,z;time="),
     ],
 )
 def test_save_file_stem(args_list, expected_stem):
