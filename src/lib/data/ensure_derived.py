5f25b9d2c586c75dd5fc9b4fdeb2c265a3eebdb5
James McClung <james@themcclungs.net>
2026-08-13 10:46:16 -0400

    ensure_derived: use SubdataKey

--- src/lib/data/ensure_derived.py
+++ src/lib/data/ensure_derived.py
@@ -1,10 +1,11 @@
 from lib.data.data_with_attrs import DataWithAttrs, Field, List
+from lib.data.types import SubdataKey
 from lib.derived_field_variables.derived_field_variable import DERIVED_FIELD_VARIABLES, derive_field_variable
 from lib.derived_particle_variables.derived_particle_variable import DERIVED_PARTICLE_VARIABLES, derive_particle_variable
 from lib.file_util import split_prepath
 
 
-def ensure_derived[D: DataWithAttrs](data: D, key: str) -> D:
+def ensure_derived[D: DataWithAttrs](data: D, key: SubdataKey) -> D:
     _, prefix = split_prepath(data.metadata.prepath)
     if isinstance(data, Field):
         return derive_field_variable(data, key, prefix)
@@ -14,7 +15,7 @@ def ensure_derived[D: DataWithAttrs](data: D, key: str) -> D:
         raise TypeError(data.__class__)
 
 
-def get_derivable_keys(data: DataWithAttrs) -> list[str]:
+def get_derivable_keys(data: DataWithAttrs) -> list[SubdataKey]:
     _, prefix = split_prepath(data.metadata.prepath)
     if isinstance(data, Field):
         return list(DERIVED_FIELD_VARIABLES[prefix].keys())
