868fc0e91c7f4d8e018cfbf71050043c77618889
James McClung <james@themcclungs.net>
2026-08-13 10:45:49 -0400

    ensure_derived: +get_derivable_keys

--- src/lib/data/ensure_derived.py
+++ src/lib/data/ensure_derived.py
@@ -1,6 +1,6 @@
 from lib.data.data_with_attrs import DataWithAttrs, Field, List
-from lib.derived_field_variables.derived_field_variable import derive_field_variable
-from lib.derived_particle_variables.derived_particle_variable import derive_particle_variable
+from lib.derived_field_variables.derived_field_variable import DERIVED_FIELD_VARIABLES, derive_field_variable
+from lib.derived_particle_variables.derived_particle_variable import DERIVED_PARTICLE_VARIABLES, derive_particle_variable
 from lib.file_util import split_prepath
 
 
@@ -10,5 +10,15 @@ def ensure_derived[D: DataWithAttrs](data: D, key: str) -> D:
         return derive_field_variable(data, key, prefix)
     elif isinstance(data, List):
         return derive_particle_variable(data, key, prefix)
+    else:
+        raise TypeError(data.__class__)
+
+
+def get_derivable_keys(data: DataWithAttrs) -> list[str]:
+    _, prefix = split_prepath(data.metadata.prepath)
+    if isinstance(data, Field):
+        return list(DERIVED_FIELD_VARIABLES[prefix].keys())
+    elif isinstance(data, List):
+        return list(DERIVED_PARTICLE_VARIABLES[prefix].keys())
     else:
         raise TypeError(data.__class__)
