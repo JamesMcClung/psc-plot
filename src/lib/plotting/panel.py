651df429f8927b5e0b7b9e60ddd3e544054b71a1
James McClung <james@themcclungs.net>
2026-08-20 08:14:16 -0400

    panel: +wire_title,legend_label

--- src/lib/plotting/panel.py
+++ src/lib/plotting/panel.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass, field
 
-from matplotlib.axes import Axes
+from matplotlib.artist import Artist
+from matplotlib.text import Text
 
 from lib.plotting.labeler import TreeLabeler
+from lib.plotting.plot_info import PlotInfo
 from lib.plotting.renderer2 import Renderer2
 
 
@@ -11,3 +13,14 @@ class Panel:
     title_labeler: TreeLabeler | None = field(init=False, default=None)
     legend_labelers: list[TreeLabeler] = field(init=False, default_factory=list)
     data_setters: list[Renderer2] = field(init=False, default_factory=list)
+
+    def wire_title(self, title: Text, info: PlotInfo | None = None):
+        self.title_labeler = TreeLabeler(title.set_text, info)
+        for legend_labeler in self.legend_labelers:
+            self.title_labeler.add_child(legend_labeler)
+
+    def wire_legend_label(self, artist: Artist, info: PlotInfo):
+        legend_labeler = TreeLabeler(artist.set_label, info)
+        self.legend_labelers.append(legend_labeler)
+        if self.title_labeler:
+            self.title_labeler.add_child(legend_labeler)
