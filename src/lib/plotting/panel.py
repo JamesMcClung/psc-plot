69e5b3b8b8a7c733f66309494a2df46c5484f5d5
James McClung <james@themcclungs.net>
2026-08-20 08:01:43 -0400

    panel: +legend_labelers

--- src/lib/plotting/panel.py
+++ src/lib/plotting/panel.py
@@ -9,4 +9,5 @@ from lib.plotting.renderer2 import Renderer2
 @dataclass
 class Panel:
     title_labeler: TreeLabeler | None = field(init=False, default=None)
+    legend_labelers: list[TreeLabeler] = field(init=False, default_factory=list)
     data_setters: list[Renderer2] = field(init=False, default_factory=list)
