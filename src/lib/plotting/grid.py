6603f12c967e3d41379d78a61994b6e68bde7a2d
James McClung <james@themcclungs.net>
2026-08-19 20:52:07 -0400

    grid: +setup_ax

--- src/lib/plotting/grid.py
+++ src/lib/plotting/grid.py
@@ -1,8 +1,13 @@
+from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 
 from lib.plotting.plot_info import PlotInfo
 
 type AxesIdx = tuple[int, int]
+
+
+def _flatten_idx(axes_idx: AxesIdx, ncols: int) -> int:
+    return ncols * (axes_idx[1] - 1) + axes_idx[0]
 
 
 class Grid:
@@ -14,3 +19,11 @@ class Grid:
 
         self.ncols = max(idx[0] for idx in self.infos)
         self.nrows = max(idx[1] for idx in self.infos)
+
+    def setup_ax(self, loc: AxesIdx) -> Axes:
+        infos = self.infos[loc]
+        projection = infos[0].projection
+        for info in infos[1:]:
+            if info.projection != projection:
+                raise ValueError("incompatible plots (TODO: better error message)")
+        return self.fig.add_subplot(self.nrows, self.ncols, _flatten_idx(loc, self.ncols), projection=projection)
