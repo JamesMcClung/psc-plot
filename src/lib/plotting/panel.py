0f9448f4bf35deb8629c1d688e4cb0bea89473c3
James McClung <james@themcclungs.net>
2026-08-20 08:21:26 -0400

    panel: +wire_cbar_label

--- src/lib/plotting/panel.py
+++ src/lib/plotting/panel.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 
 from matplotlib.artist import Artist
+from matplotlib.colorbar import Colorbar
 from matplotlib.text import Text
 
 from lib.plotting.labeler import TreeLabeler
@@ -12,15 +13,23 @@ from lib.plotting.renderer2 import Renderer2
 class Panel:
     title_labeler: TreeLabeler | None = field(init=False, default=None)
     legend_labelers: list[TreeLabeler] = field(init=False, default_factory=list)
+    cbar_labeler: TreeLabeler | None = field(init=False, default=None)
     data_setters: list[Renderer2] = field(init=False, default_factory=list)
 
     def wire_title(self, title: Text, info: PlotInfo | None = None):
         self.title_labeler = TreeLabeler(title.set_text, info)
         for legend_labeler in self.legend_labelers:
             self.title_labeler.add_child(legend_labeler)
+        if self.cbar_labeler:
+            self.title_labeler.add_child(self.cbar_labeler)
 
     def wire_legend_label(self, artist: Artist, info: PlotInfo):
         legend_labeler = TreeLabeler(artist.set_label, info)
         self.legend_labelers.append(legend_labeler)
         if self.title_labeler:
             self.title_labeler.add_child(legend_labeler)
+
+    def wire_cbar_label(self, cbar: Colorbar, info: PlotInfo):
+        self.cbar_labeler = TreeLabeler(cbar.set_label, info)
+        if self.title_labeler:
+            self.title_labeler.add_child(self.cbar_labeler)
