77cf49fdbb117d1cbfded08743a3a282e21181f6
James McClung <james@themcclungs.net>
2026-08-20 20:27:09 -0400

    panel; setup_fig: more cbar wiring

--- src/lib/plotting/panel.py
+++ src/lib/plotting/panel.py
@@ -8,7 +8,7 @@ from matplotlib.lines import Line2D
 from matplotlib.text import Text
 
 from lib.plotting.data_setter import ImageSetter, LineSetter
-from lib.plotting.labeler import SubjectAndUnitLabeler, SubjectLabeler
+from lib.plotting.labeler import Labeler, SubjectAndUnitLabeler, SubjectLabeler, UnitLabeler
 from lib.plotting.plot_info import ImageInfo, LineInfo, PlotInfo
 from lib.plotting.renderer2 import Renderer2
 
@@ -17,14 +17,14 @@ from lib.plotting.renderer2 import Renderer2
 class Panel:
     title_labeler: SubjectLabeler | None = field(init=False, default=None)
     legend_labelers: list[SubjectLabeler] = field(init=False, default_factory=list)
-    cbar_labeler: SubjectAndUnitLabeler | None = field(init=False, default=None)
+    cbar_labeler: Labeler | None = field(init=False, default=None)
     data_setters: list[Renderer2] = field(init=False, default_factory=list)
 
     def wire_title(self, title: Text, info: PlotInfo | None = None):
         self.title_labeler = SubjectLabeler(title.set_text, info)
         for legend_labeler in self.legend_labelers:
             self.title_labeler.add_child(legend_labeler)
-        if self.cbar_labeler:
+        if self.cbar_labeler and isinstance(self.cbar_labeler, SubjectAndUnitLabeler):
             self.title_labeler.add_child(self.cbar_labeler.subject_labeler)
 
     def wire_legend_label(self, artist: Artist, info: PlotInfo):
@@ -33,10 +33,13 @@ class Panel:
         if self.title_labeler:
             self.title_labeler.add_child(legend_labeler)
 
-    def wire_cbar_label(self, cbar: Colorbar, info: PlotInfo):
-        self.cbar_labeler = SubjectAndUnitLabeler(cbar.set_label, "color", info)
-        if self.title_labeler:
-            self.title_labeler.add_child(self.cbar_labeler.subject_labeler)
+    def wire_cbar_label(self, cbar: Colorbar, info: PlotInfo, *, is_subject: bool):
+        if is_subject:
+            self.cbar_labeler = SubjectAndUnitLabeler(cbar.set_label, "color", info)
+            if self.title_labeler:
+                self.title_labeler.add_child(self.cbar_labeler.subject_labeler)
+        else:
+            self.cbar_labeler = UnitLabeler(cbar.set_label, "color", [info])
 
     def setup_and_wire_image(self, ax: Axes, info: ImageInfo) -> AxesImage:
         setter = ImageSetter.setup(ax, info)
--- src/lib/plotting/setup_fig.py
+++ src/lib/plotting/setup_fig.py
@@ -117,9 +117,14 @@ class AxesManagerSingleLine(AxesManagerSingle2D[LineInfo]):
 
 
 class AxesManagerSingleImage(AxesManagerSingle2D[ImageInfo]):
+    def setup_title(self):
+        self.panel.wire_title(self.ax.title)
+
     def setup_data(self):
         image = self.panel.setup_and_wire_image(self.ax, self.info)
-        setup_colorbar(self.ax, image, self.info)
+        cbar = setup_colorbar(self.ax, image, self.info)
+        self.panel.wire_cbar_label(cbar, self.info, is_subject=True)
+        self.panel.cbar_labeler.update()
 
 
 class AxesManagerSingleScatter(AxesManagerSingle2D[ScatterInfo]):
@@ -134,7 +139,7 @@ class AxesManagerSingleScatter(AxesManagerSingle2D[ScatterInfo]):
             )
 
             cbar = setup_colorbar(self.ax, scatter, self.info)
-            self.panel.wire_cbar_label(cbar, self.info)
+            self.panel.wire_cbar_label(cbar, self.info, is_subject=False)
             self.panel.cbar_labeler.update()
         else:
             scatter = self.ax.scatter(
@@ -250,7 +255,7 @@ class AxesManagerImageAndLines(AxesManager):
     def setup_title(self):
         for info, line in zip(self.line_infos, self.lines):
             self.panel.wire_legend_label(line, info)
-        self.panel.wire_cbar_label(self.cbar, self.image_info)
+        self.panel.wire_cbar_label(self.cbar, self.image_info, is_subject=True)
         self.panel.wire_title(self.image_ax.title)
         self.panel.title_labeler.update()
 
